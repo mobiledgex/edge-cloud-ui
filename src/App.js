0be29bd16bcb145fe01320eaa8c8cc9c9ab16b2d
popkim71 <smith.lee.consultant@mobiledgex.com>
2019-12-03 16:50:10 +0900

    EDGECLOUD-1628 Web UI - messages need to wrap differently -- jiyoonjung <jiyoon.jung@bictech.co.kr>

--- src/App.js
+++ src/App.js
@@ -241,7 +241,7 @@ class App extends Component {
             if(result.data.message.indexOf('Expired') > -1) {
 
                 setTimeout(() => self.goToNext('/logout',''),4000);
-                Alert.error('Login timeout expired. Please login again', {
+                Alert.error('Login timeout expired.\nPlease login again', {
                     position: 'top-right',
                     effect: 'slide',
                     timeout: 50000
