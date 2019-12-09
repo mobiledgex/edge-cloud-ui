3023778395e991cb90f55a05c6cf1794bbe499f4
jiyoonjung <jiyoon.jung@bictech.co.kr>
2019-12-09 13:45:05 +0900

    EDGECLOUD-1590 WEB UI - Organizations Audit,Add User, and Delete buttons look to be inactive

--- src/container/insideListView.js
+++ src/container/insideListView.js
@@ -59,7 +59,9 @@ class InsideListView extends React.Component {
             isOpen: false,
             isOpenTip:false,
             actionContextRef:'actionCell_0',
-            item:null
+            item:null,
+            isHovered: false,
+            itemIndex: 0
         };
         this.sorting = false;
 
@@ -352,11 +354,22 @@ class InsideListView extends React.Component {
     makeActionButton = (target) => (
         <Button.Group vertical className="table_actions_popup_group">
             {
-                appssEdit.map((option)=> (
-                    <Button onClick={this.onHandlePopMenu} className="table_actions_popup_group_button">
+                appssEdit.map((option, index)=> {
+
+                    const btnClass =
+                        !this.state.isHovered && index === 0 ? "table_actions_popup_group_button_hover" :
+                        this.state.isHovered && this.state.itemIndex === index ? "table_actions_popup_group_button_hover" : "table_actions_popup_group_button";
+
+                    return (
+                    <Button onClick={this.onHandlePopMenu}
+                            className={btnClass}
+                            onMouseEnter={()=> this.setState({isHovered: true, itemIndex:index})}
+                            onMouseLeave={()=> this.setState({isHovered: false})}
+                    >
                         {option.text}
                     </Button>
-                ))
+                    )
+                })
             }
         </Button.Group>
     )
