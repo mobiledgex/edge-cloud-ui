d1e05378d02afffe2ee90a7ec02351139b91037a
seongjin9081 <tjdwls9081@gmail.com>
2019-12-17 09:34:15 +0900

    EDGECLOUD-1584 daily report of cloudlet usage (histogram)

--- src/container/monitoringViewer.js
+++ src/container/monitoringViewer.js
@@ -512,7 +512,7 @@ export default class MonitoringViewer extends React.Component {
                             <div style={{width:'100%', height:400}}>
                                 <Header>MEM</Header>
                                 <TimeSeries style={{width:'100%', height:200}} chartData={this.state.mProp.timeseriesDataCPUMEM} series={this.state.mProp.timeseriesCPUMEM} showLegend={true} single='1'
-                                            margin={{l: 50, r: 10, b: 45, t: 10, pad: 0}} label={this.state.mProp.dataLabel} yRange={[0.001, 0.009]} y2Position={0.94}></TimeSeries>
+                                            margin={{l: 50, r: 10, b: 45, t: 10, pad: 0}} label={this.state.mProp.dataLabel} yRange={[0.001, 0.009]} y2Position={0.94} dataType={(this.props.data.page === 'cloudlet')? 'MEM' : null}></TimeSeries>
                             </div>
                     }
 
