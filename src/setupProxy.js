b3b521722ec2f628b1a39fd8c9c3a039aeb89558
Smith.inki <popkim71@gmail.com>
2020-01-16 14:26:54 +0900

    fix setup proxy :   target:`${process.env.REACT_APP_API_ENDPOINT}`,

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,8 @@
 const proxy = require('http-proxy-middleware');
-
 module.exports = function (app) {
     app.use(proxy('/api',
         {
-            target: 'https://console-stage.mobiledgex.net:443',
+            target:`${process.env.REACT_APP_API_ENDPOINT}`,
             changeOrigin: true
         }
     ));
