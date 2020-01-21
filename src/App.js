60bd6159d29d5fd749cd3f3f74fbadae648ad428
kyungjoongo2 <kyungjoon.go@gmail.com>
2020-01-21 17:26:34 +0900

    delete unneccery files
    Signed-off-by: kyungjoongo2 <kyungjoon.go@gmail.com>

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ import SiteFour from "./sites//siteFour/siteFour";
 import './css/pages/audit.css';
 import './css/pages/monitoring.css';
 import './css/components/timelineH.css';
-import Test001 from "./TEMP_KYUNGJOOON_FOR_TEST/Pages/Test001";
 // API
 
 let self = null;
@@ -330,7 +329,6 @@ class App extends Component {
                     <Route exact path='/createAccount' component={DashboardContainer.bind(this, {mainPath:'/createAccount'})} />
                     <Route exact path='/passwordreset' component={DashboardContainer.bind(this, {mainPath:'/passwordreset'})} />
                     <Route exact path='/verify' component={DashboardContainer.bind(this, {mainPath:'/verify'})} />
-                    <Route exact path='/Test001' component={Test001} />
 
                 </div>
             </Router>
