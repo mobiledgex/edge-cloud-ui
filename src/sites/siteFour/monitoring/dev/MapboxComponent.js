e2cded8540688d44d7ba66c76d40094bb2bbfab8
kyungjoon.go2 <iamjessica77777@gmail.com>
2020-02-01 18:44:13 +0900

    add mapbox option modifyed
    Signed-off-by: kyungjoon.go2 <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/dev/MapboxComponent.js
+++ src/sites/siteFour/monitoring/dev/MapboxComponent.js
@@ -26,7 +26,7 @@ type State = {
     zoom: number,
     newCloudLetLocationList: Array,
     showModal: boolean,
-    showOffice:boolean,
+    showOffice: boolean,
 }
 
 export default class MapboxComponent extends Component<Props, State> {
@@ -35,14 +35,14 @@ export default class MapboxComponent extends Component<Props, State> {
     constructor(props) {
         super(props);
         this.state = {
-            viewport: {
-                width: '100%',
-                height: '100%',
-                latitude: 10.4515,
-                longitude: 51.1657,
-                zoom: 0.7
-            },
-            zoom: 0.75,
+            /* viewport: {
+                 width: '100%',
+                 height: '100%',
+                 latitude: 10.4515,
+                 longitude: 51.1657,
+                 zoom: 0.7
+             },*/
+            zoom: 0.9,//mapZoom
             appInstanceListGroupByCloudlet: '',
             cloudletKeys: [],
             newCloudLetLocationList: [],
@@ -155,7 +155,7 @@ export default class MapboxComponent extends Component<Props, State> {
                     //style={styles.marker}
                     coordinates={[77.595914, 12.980056]}
                     onClick={() => {
-                        showToast('BIC')
+                        showToast('Rahul')
                     }}>
                     <img
 
@@ -181,6 +181,20 @@ export default class MapboxComponent extends Component<Props, State> {
                         width: '100%',
                         height: '100%',
                     }}
+                    //movingMethod={'easeTo'}
+                    flyToOptions={{
+                        speed: 3.5,
+                        /*center: [0, 0],
+                        zoom: 45,*/
+                        /*
+
+                          curve: 3,
+                          easing(t) {
+                              return t;
+                          }*/
+                    }}
+
+
                     zoom={[this.state.zoom]}
                 >
                     {this.state.showOffice && this.renderOfficeLoc()}
@@ -193,19 +207,19 @@ export default class MapboxComponent extends Component<Props, State> {
                                 //key={key}
                                 //style={styles.marker}
                                 coordinates={[item.CloudletLocation.longitude, item.CloudletLocation.latitude]}
-                                onClick={() => {
-                                    showToast(item.AppNames + "[" + item.Cloudlet + "]")
-                                }}>
+                            >
                                 <img
                                     src="https://cdn1.iconfinder.com/data/icons/basic-ui-elements-coloricon/21/06_1-512.png" style={{color: 'red'}} height="30" width="25"/>
 
                                 <div style={{color: 'yellow', fontWeight: 'bold', fontSize: 15, fontFamily: 'Acme'}}>
                                     [{item.Cloudlet}]
                                 </div>
-                                {listAppName.map(item => {
+                                {listAppName.map(AppName => {
                                     return (
-                                        <div style={{color: 'white', fontSize: 12, fontFamily: 'Acme'}}>
-                                            {item}
+                                        <div style={{color: 'white', fontSize: 12, fontFamily: 'Acme'}} onClick={() => {
+                                            showToast(AppName)
+                                        }}>
+                                            {AppName}
                                         </div>
                                     )
                                 })}
@@ -253,7 +267,7 @@ export default class MapboxComponent extends Component<Props, State> {
                     }}>
                         <Icon name="minus square outline"/>
                     </Button>
-                      <Button id="mapZoomCtl" size='large' icon onClick={() => {
+                    <Button id="mapZoomCtl" size='large' icon onClick={() => {
                         this.setState({
                             showOffice: !this.state.showOffice,
                         })
