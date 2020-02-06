176aa6cf4c068a1feb63c3ac4a37146cb637ab14
kyungjoongo2 <kyungjoon.go@gmail.com>
2020-02-06 17:12:08 +0900

    add testcode
    Signed-off-by: kyungjoongo2 <kyungjoon.go@gmail.com>

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import './css/pages/audit.css';
 import './css/pages/cloudletPool.css';
 import './css/pages/monitoring.css';
 import './css/components/timelineH.css';
+import LeafletTest001 from "./sites/siteFour/monitoring/temp/TEMP_KYUNGJOOON_FOR_TEST/Pages/LeafletTest001";
 // API
 
 let self = null;
@@ -330,6 +331,7 @@ class App extends Component {
                     <Route exact path='/createAccount' component={DashboardContainer.bind(this, {mainPath:'/createAccount'})} />
                     <Route exact path='/passwordreset' component={DashboardContainer.bind(this, {mainPath:'/passwordreset'})} />
                     <Route exact path='/verify' component={DashboardContainer.bind(this, {mainPath:'/verify'})} />
+                    <Route exact path='/LeafletTest001' component={<LeafletTest001/>} />
                 </div>
             </Router>
         );
