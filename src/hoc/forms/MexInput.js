9e9dc472c1e477e75b73f40933a1f4d3369bc661
rahuldsce <rahul.jain@mobiledgex.com>
2020-02-12 20:06:30 +0530

    Edgecloud 2031 Web UI - Privacy Policy should not show port fields for ICMP (#139)
    * Fixed EDGECLOUD-2031
    
    * Fixed EDGECLOUD-2017

--- src/hoc/forms/MexInput.js
+++ src/hoc/forms/MexInput.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Form, Icon, Popup } from 'semantic-ui-react';
 const MexInput = (props) => {
 
+    let form = props.form
+
     const [value, setValue] = React.useState(props.form.value ? props.form.value : '')
     
-    const onValueChange = (form, value) =>
+    const onValueChange = (value) =>
     {
         setValue(value)
         props.onChange(form, value, props.parentForm)
     }
-    let form = props.form
 
     const getForm = () => (
         <div>
@@ -17,7 +18,7 @@ const MexInput = (props) => {
                 icon={form.error ? <Icon color='red' name='times circle outline'/> : null}
                 label={props.label ? props.label : null}
                 placeholder={form.placeholder ? form.placeholder : null}
-                onChange={(e, { value }) => onValueChange(form, value)}
+                onChange={(e, { value }) => onValueChange(value)}
                 type={form.rules ? form.rules.type : 'text'}
                 required={form.required ? form.rules.required : false}
                 disabled={props.disabled}
