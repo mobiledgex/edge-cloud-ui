65e32457c0143e417a0a950255a7366bdb201da3
kyungjoon.go2 <iamjessica77777@gmail.com>
2020-02-14 16:27:33 +0900

    remove 테스트 files
    Signed-off-by: kyungjoon.go2 <iamjessica77777@gmail.com>

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ import './css/pages/audit.css';
 import './css/pages/cloudletPool.css';
 import './css/pages/monitoring.css';
 import './css/components/timelineH.css';
-import Test003 from "./sites/siteFour/monitoring/temp/TEMP_KYUNGJOOON_FOR_TEST/Pages/Test003";
 // API
 
 let self = null;
@@ -331,7 +330,6 @@ class App extends Component {
                     <Route exact path='/createAccount' component={DashboardContainer.bind(this, {mainPath:'/createAccount'})} />
                     <Route exact path='/passwordreset' component={DashboardContainer.bind(this, {mainPath:'/passwordreset'})} />
                     <Route exact path='/verify' component={DashboardContainer.bind(this, {mainPath:'/verify'})} />
-                    <Route exact path='/Test003' component={Test003} />
 
                 </div>
             </Router>
