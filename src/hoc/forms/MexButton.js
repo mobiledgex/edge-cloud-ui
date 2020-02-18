163ddca87401f79846b74b8d9ae8f22558264f55
rahuldsce <rahul.jain@mobiledgex.com>
2020-02-18 09:20:26 +0530

    Edgecloud 2076 - Docker Log Implementation (#154)
    * Docker Log Implementation

---
+++ src/hoc/forms/MexButton.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Form } from 'semantic-ui-react';
+
+
+const MexButton = (props)=>
+{
+    let form  = props.form;
+
+    const getForms = () => (
+        <Form.Button
+            positive = {form.style ? false : true}
+            style = {form.style}
+            content={form.label}
+            onClick={(e) => { props.onClick(form)}}
+        />
+    )
+
+    return (
+        form ?
+            getForms() : null 
+    )
+}
+
+export default MexButton
