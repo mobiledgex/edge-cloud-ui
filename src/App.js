a6d160e97774493af1cfdc219617c4ce6dc7992a
kyungjoon.go2 <iamjessica77777@gmail.com>
2020-02-28 18:18:25 +0900

    unneccery code removing#2
    Signed-off-by: kyungjoon.go2 <iamjessica77777@gmail.com>

--- src/App.js
+++ src/App.js
@@ -333,10 +333,6 @@ class App extends Component {
                     <Route exact path='/createAccount' component={DashboardContainer.bind(this, {mainPath:'/createAccount'})} />
                     <Route exact path='/passwordreset' component={DashboardContainer.bind(this, {mainPath:'/passwordreset'})} />
                     <Route exact path='/verify' component={DashboardContainer.bind(this, {mainPath:'/verify'})} />
-                    <Route exact path='/PageModalMonitoring' component={PageModalMonitoring} />
-                    <Route exact path='/Test004' component={Test004} />
-                    <Route exact path='/Test003' component={Test003} />
-
                 </div>
             </Router>
         );
