7af76ea04adc1048365ae06c9dd26ad3cf23bb76
Rahul.Jain <rahul.jain@mobiledgex.com>
2020-03-05 18:19:47 +0530

    Implemented Listview for ClusterInstance

--- src/services/model/clusterInstance.js
+++ src/services/model/clusterInstance.js
@@ -1,5 +1,6 @@
 import { fields, formatData } from './format'
 import * as constant from './shared';
+import { TYPE_JSON } from '../../hoc/constant';
 
 export const keys = [
     { field: fields.region, label: 'Region', sortable: true, visible: true },
@@ -9,11 +10,12 @@ export const keys = [
     { field: fields.cloudletName, serverField: 'key#OS#cloudlet_key#OS#name', label: 'Cloudlet', visible: true },
     { field: fields.flavorName, serverField: 'flavor#OS#name', label: 'Flavor', visible: true },
     { field: fields.ipAccess, serverField: 'ip_access', label: 'IP Access', visible: true, customizedData: constant.getIPAccess },
-    { field: fields.nodeFlavor, serverField: 'node_flavor' },
-    { field: fields.numberOfMasters, serverField: 'num_masters' },
-    { field: fields.numberOfNodes, serverField: 'num_nodes' },
+    { field: fields.cloudletLocation, label: 'Cloudlet Location', dataType: TYPE_JSON },
+    { field: fields.nodeFlavor, serverField: 'node_flavor', label:'Node Flavor'},
+    { field: fields.numberOfMasters, serverField: 'num_masters', label:'Number of Masters'},
+    { field: fields.numberOfNodes, serverField: 'num_nodes', label:'Node of Nodes' },
     { field: fields.state, serverField: 'state', label: 'Progress', visible: true, customizedData: constant.showProgress, clickable: true },
-    { field: fields.status, serverField: 'status' },
+    { field: fields.status, serverField: 'status', label:'Status', dataType:TYPE_JSON },
     { field: fields.reservable, serverField: 'reservable' },
     { field: fields.deployment, serverField: 'deployment', label: 'Deployment', visible: true },
     { field: fields.actions, label: 'Actions', sortable: false, visible: true, clickable: true }
