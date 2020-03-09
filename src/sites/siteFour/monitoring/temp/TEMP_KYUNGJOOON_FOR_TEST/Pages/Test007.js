25b516bb6800e84efc7244b83c831603dd8075e5
kyungjoon.go2 <iamjessica77777@gmail.com>
2020-03-09 15:41:54 +0900

    bug fixed#2dsfsdsdf
    Signed-off-by: kyungjoon.go2 <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/temp/TEMP_KYUNGJOOON_FOR_TEST/Pages/Test007.js
+++ src/sites/siteFour/monitoring/temp/TEMP_KYUNGJOOON_FOR_TEST/Pages/Test007.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { WidthProvider, Responsive } from "react-grid-layout";
+import {WidthProvider, Responsive} from "react-grid-layout";
+import * as d3chromatic from 'd3-scale-chromatic';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const originalLayouts = getFromLS("layouts") || {};
+
 
 /**
  * This layout demonstrates how to sync multiple responsive layouts to localstorage.
@@ -16,14 +18,18 @@ export default class Test007 extends React.PureComponent {
         };
     }
 
+    async componentDidMount(): void {
+
+    }
+
 
     resetLayout() {
-        this.setState({ layouts: {} });
+        this.setState({layouts: {}});
     }
 
     onLayoutChange(layout, layouts) {
         saveToLS("layouts", layouts);
-        this.setState({ layouts });
+        this.setState({layouts});
     }
 
     render() {
@@ -32,26 +38,26 @@ export default class Test007 extends React.PureComponent {
                 <button onClick={() => this.resetLayout()}>Reset Layout</button>
                 <ResponsiveReactGridLayout
                     className="layout"
-                    cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
+                    cols={{lg: 12, md: 10, sm: 6, xs: 4, xxs: 2}}
                     rowHeight={30}
                     layouts={this.state.layouts}
                     onLayoutChange={(layout, layouts) =>
                         this.onLayoutChange(layout, layouts)
                     }
                 >
-                    <div style={{backgroundColor:'blue'}} key="1" data-grid={{ w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3 }}>
+                    <div style={{backgroundColor: 'blue'}} key="1" data-grid={{w: 2, h: 3, x: 0, y: 0, minW: 2, minH: 3}}>
                         <span className="text">1</span>
                     </div>
-                    <div style={{backgroundColor:'blue'}}  key="2" data-grid={{ w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3 }}>
+                    <div style={{backgroundColor: 'blue'}} key="2" data-grid={{w: 2, h: 3, x: 2, y: 0, minW: 2, minH: 3}}>
                         <span className="text">2</span>
                     </div>
-                    <div  style={{backgroundColor:'blue'}} key="3" data-grid={{ w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3 }}>
+                    <div style={{backgroundColor: 'blue'}} key="3" data-grid={{w: 2, h: 3, x: 4, y: 0, minW: 2, minH: 3}}>
                         <span className="text">3</span>
                     </div>
-                    <div  style={{backgroundColor:'blue'}} key="4" data-grid={{ w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3 }}>
+                    <div style={{backgroundColor: 'blue'}} key="4" data-grid={{w: 2, h: 3, x: 6, y: 0, minW: 2, minH: 3}}>
                         <span className="text">4</span>
                     </div>
-                    <div  style={{backgroundColor:'blue'}} key="5" data-grid={{ w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3 }}>
+                    <div style={{backgroundColor: 'blue'}} key="5" data-grid={{w: 2, h: 3, x: 8, y: 0, minW: 2, minH: 3}}>
                         <span className="text">5</span>
                     </div>
                 </ResponsiveReactGridLayout>
