f52c92b344923bf190fce364315832da661efcac
kyungjoon.go2 <iamjessica77777@gmail.com>
2020-03-10 16:31:11 +0900

    add context menu test code
    Signed-off-by: kyungjoon.go2 <iamjessica77777@gmail.com>

---
+++ src/sites/siteFour/monitoring/temp/TEMP_KYUNGJOOON_FOR_TEST/Pages/Test010.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Menu, Item, Separator, Submenu, MenuProvider} from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.min.css';
+import {theme, animation} from 'react-contexify';
+
+type Props = {};
+type State = {};
+
+export default class Test010 extends React.Component<Props, State> {
+    render() {
+        return (
+            <div>
+                <div style={{margin: 50}}>
+                    <MenuProvider id="context1" style={{border: '1px solid purple', display: 'inline-block'}}>
+                        Right click me...
+                    </MenuProvider>
+                    <ContextMenu1/>
+                </div>
+            </div>
+        );
+    };
+};
+
+const onClick = ({event, props}) => {
+    alert('sdlfksdlkflskdflk')
+};
+
+// create your menu first
+const ContextMenu1 = () => (
+    <Menu id='context1' theme={theme.dark} animation={animation.pop}>
+        <Item onClick={onClick} style={{color: 'white'}}>Lorem</Item>
+        <Item onClick={onClick} style={{color: 'white'}}>Lorem2</Item>
+        <Item onClick={onClick} style={{color: 'white'}}>Lorem3</Item>
+        <Item onClick={onClick} style={{color: 'white'}}>Lorem4</Item>
+    </Menu>
+);
