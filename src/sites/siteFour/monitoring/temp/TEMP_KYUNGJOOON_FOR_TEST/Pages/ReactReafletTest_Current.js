18960451cebc8e68b8a5290fe2b5585e987fb465
kyungjoongo2 <58411936+kyungjoongo2@users.noreply.github.com>
2020-03-17 11:32:24 +0900

    add map test codes
    Signed-off-by: kyungjoongo2 <58411936+kyungjoongo2@users.noreply.github.com>

--- src/sites/siteFour/monitoring/temp/TEMP_KYUNGJOOON_FOR_TEST/Pages/ReactReafletTest_Current.js
+++ src/sites/siteFour/monitoring/temp/TEMP_KYUNGJOOON_FOR_TEST/Pages/ReactReafletTest_Current.js
@@ -12,11 +12,21 @@ import L from 'leaflet';
 import {hot} from "react-hot-loader/root";
 
 import '../../../PageMonitoring.css'
+import {Button} from "antd";
+import {Select} from "antd";
+import {Icon} from "semantic-ui-react";
 
 
 type Props = {};
 type State = {
-    visible: boolean
+    visible: boolean,
+    currentTarget: any,
+    frankuTarget: any,
+    currentTarget: any,
+    darkTileLayer: any,
+    whiteTileLayer: any,
+    currentTyleLayer: any,
+    currentZoomLevel: number,
 };
 let greenIcon = new L.Icon({
     iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
@@ -56,16 +66,43 @@ var cell_phone = L.icon({
 });
 
 export default hot(
-    class
-         extends React.Component<Props, State> {
+    class ReactReafletTest extends React.Component<Props, State> {
         map = null;
 
         state = {
             visible: false,
+            busanTarget: [35.1796, 129.0756],
+            frankuTarget: [50.1109, 8.6821],
+            currentTarget: [50.1109, 8.6821],
+            currentZoomLevel: 3,
+            /*darkTileLayer: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+            whiteTileLayer: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png',*/
+            currentTyleLayer: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
         }
 
-        componentDidMount(): void {
+        mapTileList = [
+            {
+                url: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+                name: 'dark1',
+            },
+            {
+                url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
+                name: 'dark2',
+            },
 
+            {
+                url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+                name: 'white1',
+            },
+            {
+                url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+                name: 'white2',
+            },
+
+
+        ]
+
+        componentDidMount(): void {
 
         }
 
@@ -81,18 +118,29 @@ export default hot(
 
             return (
                 <div className='page_monitoring_container'>
-                    <div style={{height: '100%', width: '100%', zIndex: 1}}>
+                    <div style={{height: '70%', width: '100%', zIndex: 1}}>
                         <Map center={[45.4, 51.7]}
                              duration={0.9}
-                             style={{width: '100%', height: '100%', zIndex: 1,}}
-                             zoom={3}
+                            //style={{width: '100%', height: '100%', zIndex: 1,}}
+                             zoom={this.state.currentZoomLevel}
+                             onZoomEnd={(e) => {
+
+                                 let zoomLevel = e.target._zoom;
+
+                                 this.setState({
+                                     currentZoomLevel: zoomLevel,
+                                 })
+                             }}
+                             ref={(ref) => {
+                                 this.map = ref;
+                             }}
                              easeLinearity={1}
                              useFlyTo={true}
                              dragging={true}
                              boundsOptions={{padding: [50, 50]}}
                         >
                             <TileLayer
-                                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+                                url={this.state.currentTyleLayer}
                                 //url={'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'}
                                 minZoom={2}
                                 style={{zIndex: 1}}
@@ -107,12 +155,17 @@ export default hot(
                             </Marker>
 
                             <Marker
+                                position={[35.1796, 129.0756]}
+                                icon={cloud}
+                            >
+                                <Popup>부산 클라우드렛.</Popup>
+                            </Marker>
+                            <Marker
                                 position={[37.5665, 126.9780]}
                                 icon={cell_phone}
                             >
                                 <Popup>seoul WeWork.</Popup>
                             </Marker>
-
                             <Marker
 
                                 position={[37.3947, 127.1112]}
@@ -133,29 +186,92 @@ export default hot(
                                 <Popup>용인 jessica.</Popup>
                             </Marker>
                             <Polyline
-                                dashArray={['10, 10']}
+                                dashArray={['8, 8']}
                                 positions={[
-                                    [50.1109, 8.6821], [37.2411, 127.1776],
+                                    this.state.currentTarget, [37.2411, 127.1776],
                                 ]}
-                                color={'grey'}
+                                color={'yellow'}
                             />
                             <Polyline
-                                dashArray={['10, 10']}
+                                dashArray={['8, 8']}
                                 positions={[
-                                    [50.1109, 8.6821], [37.3947, 127.1112],
+                                    this.state.currentTarget, [37.3947, 127.1112],
                                 ]}
-                                color={'grey'}
+                                color='yellow'
                             />
                             <Polyline
-                                dashArray={['10, 10']}
+                                dashArray={['8, 8']}
                                 positions={[
-                                    [50.1109, 8.6821], [37.5665, 126.9780],
+                                    this.state.currentTarget, [37.5665, 126.9780],
                                 ]}
-                                color={'grey'}
+                                color={'yellow'}
                             />
 
 
                         </Map>
+                        <div style={{display: 'flex', marginTop: 30, marginLeft: 10,}}>
+                            <Button type="info"
+                                    onClick={() => {
+                                        this.setState({
+                                            currentTarget: this.state.frankuTarget,
+                                        })
+                                    }}
+                            >changeTarget2</Button>
+                            <div style={{width: 30}}/>
+                            <Button type="primary"
+                                    onClick={() => {
+                                        this.setState({
+                                            currentTarget: this.state.busanTarget,
+                                        })
+                                    }}
+                            >changeTarget</Button>
+                            <div style={{width: 30}}/>
+                            <Button type="primary"
+                                    style={{backgroundColor: 'orange', borderColor: 'orange'}}
+                                    onClick={() => {
+                                        this.setState({
+                                            currentZoomLevel: 3,
+                                        })
+                                    }}
+                            >reset zoom level</Button>
+
+                            <div style={{width: 30}}/>
+                            <Select defaultValue={this.mapTileList[0].name} style={{width: 120}} onChange={(value) => {
+
+                                this.setState({
+                                    currentTyleLayer: this.mapTileList[value].url
+                                })
+
+                            }}>
+                                {this.mapTileList.map((item, index) => {
+                                    return (
+                                        <Select.Option value={index}>{item.name}</Select.Option>
+                                    )
+                                })}
+
+
+                            </Select>
+                            <div style={{position: 'absolute', top: 75, left: 12, zIndex:99999}}>
+                                <Icon
+
+                                    onClick={() => {
+                                        this.setState({
+                                            currentZoomLevel: 3,
+                                        })
+                                    }}
+                                    name='redo '
+                                    style={{
+                                        color: 'black',
+                                        fontSize: 20,
+                                        backgroundColor: 'white',
+                                        height: 30,
+                                        width: 30
+                                    }}/>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+
                     </div>
                 </div>
             );
