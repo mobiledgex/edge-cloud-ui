4a812267900f6dbe291d66f390f9e1f8a4ac190e
rahuldsce <rahul.jain@mobiledgex.com>
2020-03-29 15:50:17 +0530

    User can modify deployment manifest (#205)

--- src/hoc/forms/MexTextArea.js
+++ src/hoc/forms/MexTextArea.js
@@ -12,6 +12,7 @@ const MexInput = (props) => {
 
     const getForm = () => (
         <TextArea
+            rows = {5}
             icon={form.error ? <Icon color='red' name='times circle outline' /> : null}
             label={props.label ? props.label : null}
             placeholder={form.placeholder ? form.placeholder : null}
@@ -20,7 +21,7 @@ const MexInput = (props) => {
             required={form.required ? form.rules.required : false}
             disabled={props.disabled}
             value={value}
-            style={form.style ? form.style : {backgroundColor:'#18191E', color:'#FFF'}}
+            style={form.style ? form.style : {backgroundColor:'#18191E', color:'#939396'}}
         />
     )
     return (
