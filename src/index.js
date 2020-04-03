1ada32bcaedef14abfe1309b0c63039763d3b538
kyungjoongo3 <iamjessica77777@gmail.com>
2020-04-04 07:49:26 +0900

    disable cache
    Signed-off-by: kyungjoongo3 <iamjessica77777@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { unregister } from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 import store from './store';
-import { Provider } from 'react-redux';
-
+import {Provider} from 'react-redux';
 import './charts/sparkline.css';
 import '../node_modules/react-grid-layout/css/styles.css';
 import '../node_modules/react-resizable/css/styles.css';
-
-
 unregister();
+
 ReactDOM.render(
-    <Provider store={store} style={{width:'100%', height:'100%'}}><App /></Provider>,
-    document.getElementById('root'));
+    <Provider store={store} style={{width: '100%', height: '100%'}}><App/></Provider>,
+    document.getElementById('root')
+);
 //registerServiceWorker();
