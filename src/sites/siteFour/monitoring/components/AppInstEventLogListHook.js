d17ece2103f45c876714674c2feb71350827b68c
kyungjoongo3 <iamjessica77777@gmail.com>
2020-04-09 18:04:24 +0900

    source adjustment #2
    Signed-off-by: kyungjoongo3 <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/components/AppInstEventLogListHook.js
+++ src/sites/siteFour/monitoring/components/AppInstEventLogListHook.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import PageDevMonitoring from "../dev/PageDevMonitoring";
 import TableContainer from "@material-ui/core/TableContainer";
 import Paper from "@material-ui/core/Paper";
