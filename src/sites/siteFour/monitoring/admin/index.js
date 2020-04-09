529d14b6395c2dc63579d4ac532c381c7b9415ef
Smith.inki <popkim71@gmail.com>
2020-04-09 09:11:40 +0900

    grid handle draggable header

--- src/sites/siteFour/monitoring/admin/index.js
+++ src/sites/siteFour/monitoring/admin/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import _ from "lodash";
-import MonitoringLayout from "./layout";
+import MonitoringLayout from "../layout/layout";
 //
 import TestWidget from "../container/TestWidget";
 
@@ -44,12 +44,6 @@ export default class MonitoringAdmin extends React.Component {
  * You can get layout value in here..  try it.
  * https://strml.github.io/react-grid-layout/examples/6-dynamic-add-remove.html
  * Displayed as [x, y, w, h]:
-0: [0, 0, 3, 2]
-1: [0, 2, 3, 2]
-2: [3, 0, 6, 4]
-3: [9, 0, 3, 2]
-4: [9, 2, 3, 2]
-5: [0, 4, 12, 2]
  */
 let headerTitle = ["Title1", "Title2", "Title3", "Title4", "Title5", "Title6"];
 let itemX = [0, 0, 3, 3 + 6, 3 + 6, 0];
---
+++ src/sites/siteFour/monitoring/components/index.js
@@ -0,0 +1,1 @@
+export TimeSeries from "../../../../charts/plotly/timeSeries";
