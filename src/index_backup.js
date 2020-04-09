4af57ff4c5c83d21261ee6aae9bcf2faec836381
kyungjoongo3 <iamjessica77777@gmail.com>
2020-04-09 09:59:37 +0900

    adjusted header size
    Signed-off-by: kyungjoongo3 <iamjessica77777@gmail.com>

---
+++ src/index_backup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './charts/sparkline.css';
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
+import App from './App';
+import store from './store';
+import {Provider} from 'react-redux';
+
+ReactDOM.render(
+    <Provider store={store} style={{width: '100%', height: '100%'}}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
+
+
+
