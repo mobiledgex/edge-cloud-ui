ddd37cdb79388da332799d2afade9614d0843d7a
kyungjoongo2 <58411936+kyungjoongo2@users.noreply.github.com>
2020-04-14 17:42:26 +0900

    EDGECLOUD-2475, EDGECLOUD-2473 (#266)
    EDGECLOUD-2475
    [WebUI-Monitoring] Bars representation in Performance Summary is not accurate fir MEM and CPU section
    EDGECLOUD-2473
    [WebUI-Monitoring] Graph doesn't represent live data in full window when Streaming is ON
    EDGECLOUD-2467
    [WebUI-Monitoring] Timing (Y-Axis) is not present for any graphs in V1.1.109
    EDGECLOUD-2453
    [Web UI-Monitoring] Graph name for Active, Handled and Accept Connections are overlapping with maximize window icon
    --ETC (Sorry, it was not fixed in the previous version.)
    Fixed dragging bug of grid item at the AppInst level (edited)

--- src/sites/siteFour/monitoring/components/LineChartContainer.js
+++ src/sites/siteFour/monitoring/components/LineChartContainer.js
@@ -52,13 +52,43 @@ export default class LineChartContainer extends React.Component<Props, State> {
     }
 
 
+    makeToShortTitle(hwType) {
+        let title = hwType.replace("_", "")
+        if (title.includes('ACTIVECONNECTION')) {
+            return 'ACTIVE CONN'
+        } else if (title.includes('ACTIVECONNECTION')) {
+            return 'ACTIVE CONN'
+        } else if (title.includes('HANDLEDCONNECTION')) {
+            return 'HANDLED CONN'
+        } else if (title.includes('ACCEPTSCONNECTION')) {
+            return 'ACCEPTS CONN'
+        } else if (title.includes('TCPRETRANS')) {
+            return 'TCP RETRANS'
+        } else if (title.includes('TCPCONNS')) {
+            return 'TCP CONNS'
+        } else if (title.includes('UDPRECV')) {
+            return 'UDP RECV'
+        } else if (title.includes('UDPSENT')) {
+            return 'UDP SENT'
+        } else if (title.includes('RECVBYTES')) {
+            return 'RECV BYTES'
+        } else if (title.includes('SENDBYTES')) {
+            return 'SEND BYTES'
+        } else {
+            return title
+        }
+
+
+    }
+
+
     render() {
         return (
             <div className='page_monitoring_dual_column' style={{display: 'flex'}}>
                 <div className='page_monitoring_dual_container' style={{flex: 1}}>
                     <div className='page_monitoring_title_area draggable' style={{backgroundColor: 'transparent'}}>
                         <div className='page_monitoring_title' style={{fontFamily: 'Ubuntu'}}>
-                            {convertToClassification(this.props.currentClassification)} {this.state.pHardwareType !== undefined && this.state.pHardwareType.replace("_", "")} Usage
+                            {convertToClassification(this.props.currentClassification)} {this.state.pHardwareType !== undefined && this.makeToShortTitle(this.state.pHardwareType)} Usage
                         </div>
                     </div>
                     <div className='page_monitoring_container'>
