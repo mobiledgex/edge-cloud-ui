f1800e6ea06771909b021f4f2e3c107bfad0f6f0
kyungjoongo3 <iamjessica77777@gmail.com>
2020-04-16 20:39:32 +0900

    [ADD TEST CODES FOR DEV MON....]
    Signed-off-by: kyungjoongo3 <iamjessica77777@gmail.com>

---
+++ src/sites/siteFour/monitoring/aaa____TESTCODE____/Pages/TestLeafLetMapRaw.js
@@ -0,0 +1,126 @@
+import 'react-hot-loader';
+import * as React from 'react';
+import L from 'leaflet';
+import {SemanticToastContainer} from "react-semantic-toasts";
+import {showToast} from "../../PageMonitoringCommonService";
+import {Modal as AModal} from "antd";
+import './styles.css'
+import {hot} from "react-hot-loader/root";
+import Button from "@material-ui/core/Button";
+import $ from 'jquery';
+
+type Props = {};
+type State = {
+    visible: boolean
+};
+var greenIcon = L.icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+    //shadowUrl : 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+    iconSize: [25, 45], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var redIcon = L.icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    //shadowUrl : 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+    iconSize: [25, 45], // size of the icon
+    shadowSize: [50, 64], // size of the shadow
+    iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+export default hot(
+    class TestLeafLetMapRaw extends React.Component<Props, State> {
+        map=null;
+
+        state = {
+            visible: false,
+        }
+
+        componentDidMount(): void {
+
+            $(document).ready(function () {
+
+            });
+
+
+            this.map = L.map('map').setView([37.3501, 127.1090], 13,);
+
+            //url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+
+            //var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+            var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+                subdomains: 'abcd',
+                maxZoom: 15
+            }).addTo(this.map);
+
+            let point2 = L.marker([37.3501, 127.1090], {
+                icon: greenIcon,
+            }).addTo(this.map).on('click', () => {
+                showToast(point2.getLatLng().toString())
+                /*this.setState({
+                    visible: !this.state.visible
+                })*/
+            })
+
+
+            let point3 = L.marker([37.3501, 128.1090], {
+                icon: greenIcon,
+            }).addTo(this.map).on('click', () => {
+                showToast(point2.getLatLng().toString())
+                /*this.setState({
+                    visible: !this.state.visible
+                })*/
+            })
+
+
+            /*L.tileLayer('"https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);*/
+
+            this.map.doubleClickZoom.disable();
+        }
+
+        render() {
+            return (
+                <div style={{flex: 1, height: '100%'}}>
+                    <AModal
+                        mask={false}
+                        title="Basic Modal"
+                        visible={this.state.visible}
+                        onOk={() => {
+                            this.setState({
+                                visible: false,
+                            })
+                        }}
+                        onCancel={() => {
+                            this.setState({
+                                visible: false,
+                            })
+                        }}
+                        width={'100%'}
+                    >
+                        <p>Some contents...</p>
+                        <p>Some contents...</p>
+                        <p>Some contents...</p>
+                    </AModal>
+                    <SemanticToastContainer position={"top-right"}/>
+                    <div id="map" style={{height: 800}}>xx</div>
+                    <Button id='btn1' style={{backgroundColor: 'red', color: 'white'}} onClick={() => {
+
+                        L.marker([37.3501, 127.5090], {
+                            icon: redIcon,
+                        }).addTo(this.map)
+                    }}>
+                        redIcon
+                    </Button>
+                </div>
+            );
+        };
+    }
+)
---
+++ src/sites/siteFour/monitoring/aaa____TESTCODE____/Pages/WebSocketTestForClusterInst.js
@@ -0,0 +1,180 @@
+import React, {Component} from 'react';
+import {Line} from 'react-chartjs-2';
+import {hot} from "react-hot-loader/root";
+
+const data2222 = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: 'My First dataset2',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(192,7,9,0.4)',
+            borderColor: 'rgb(192,7,9)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [15, 12, 39, 40, 56, 55, 40]
+        }
+    ]
+};
+
+const data = {
+    datasets: [
+        {
+            label: "Dataset 1",
+            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            lineTension: 0,
+            borderDash: [8, 4],
+            data: [15, 12, 39, 40, 56, 55, 40]
+
+        }
+    ]
+};
+
+const options = {
+    scales: {
+        xAxes: [
+            {
+                type: "realtime",
+                realtime: {
+                    onRefresh: function() {
+                        data2222.datasets[0].data.push({
+                            x: Date.now(),
+                            y: Math.random() * 100
+                        });
+                    },
+                    delay: 1000
+                }
+            }
+        ]
+    }
+};
+
+export default hot(
+    class WebSocketTestForClusterInst extends Component {
+
+        componentDidMount() {
+            /*const {datasets} = this.refs.chart.chartInstance.data
+            console.log(datasets[0].data);*/
+
+            this.getGrapHData();
+        }
+
+        _showAppInstClientRequestForm =  {
+            "region": "EU",
+            "clusterinst": {
+                "cluster_key": {
+                    "name": "mexdemo-stage"
+                },
+                "cloudlet_key": {
+                    "organization": "TDG",
+                    "name": "mexplat-stage-hamburg-cloudlet"
+                },
+                "organization": "MobiledgeX"
+            },
+            "last": 50,
+            "selector": "*"
+        }
+
+
+        getGrapHData(){
+            let prefixUrl = (process.env.REACT_APP_API_ENDPOINT).replace('http', 'ws');
+            console.log("onmessage==REACT_APP_API_ENDPOINT==>", prefixUrl)
+
+            const webSocket = new WebSocket(`${prefixUrl}/ws/api/v1/auth/metrics/cluster`)
+
+            console.log("webSocket===>", webSocket);
+
+            let showAppInstClientRequestForm =  {
+                "region": "EU",
+                "clusterinst": {
+                    "cluster_key": {
+                        "name": "mexdemo-stage"
+                    },
+                    "cloudlet_key": {
+                        "organization": "TDG",
+                        "name": "mexplat-stage-hamburg-cloudlet"
+                    },
+                    "organization": "MobiledgeX"
+                },
+                "last": 50,
+                "selector": "*"
+            }
+
+            let store = JSON.parse(localStorage.PROJECT_INIT);
+            let token = store ? store.userToken : 'null';
+
+
+            webSocket.onopen = () => {
+                try{
+                    webSocket.send(JSON.stringify({token: token}))
+                    webSocket.send(JSON.stringify(showAppInstClientRequestForm))
+                }catch (e) {
+                    alert(e)
+                }
+            }
+
+
+            webSocket.onmessage = async (event) => {
+
+                let data = JSON.parse(event.data);
+
+                console.log("onmessage===>", data);
+            }
+
+
+            webSocket.onclose = function (event) {
+                //alert(event.toString())
+                alert('onclose')
+            };
+        }
+
+
+        render() {
+            return (
+                <div style={{backgroundColor:'white'}}>
+                    <h2>Line Example</h2>
+                    <div style={{width: 1500, height: 1500}}>
+                        <Line data={data2222} options={options}  />
+                    </div>
+
+                </div>
+            );
+        }
+
+
+    }
+)
