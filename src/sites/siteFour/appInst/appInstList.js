7aa16ea9fa4ecbd3b162868b57ed3e11ca73947c
Rahul.Jain <rahul.jain@mobiledgex.com>
2020-04-21 00:30:26 +0530

    VM Console support for appInst

--- src/sites/siteFour/appInst/appInstList.js
+++ src/sites/siteFour/appInst/appInstList.js
@@ -9,7 +9,8 @@ import { fields } from '../../../services/model/format';
 import { keys, showAppInsts, deleteAppInst, streamAppInst, refreshAppInst, multiDataRequest } from '../../../services/model/appInstance';
 import { showApps } from '../../../services/model/app';
 import AppInstReg from './appInstReg';
-import * as constant from '../../../services/model/shared';
+import * as constant from '../../../constant';
+import * as shared from '../../../services/model/shared';
 import TerminalViewer from '../../../container/TerminalViewer';
 import * as serviceMC from '../../../services/serviceMC';
 import { Dialog } from '@material-ui/core';
@@ -40,55 +41,24 @@ class AppInstList extends React.Component {
     onTerminalVisible = (data) => {
         let visible = false;
         if (data) {
-            let runtimeInfo = data[fields.runtimeInfo]
-            if (runtimeInfo) {
-                let containers = runtimeInfo[fields.container_ids]
-                if (containers && containers.length > 0) {
-                    visible = true
-                }
+            if (data[fields.deployment] === constant.DEPLOYMENT_TYPE_VM) {
+                visible = true
             }
+            // else {
+            //     let runtimeInfo = data[fields.runtimeInfo]
+            //     if (runtimeInfo) {
+            //         let containers = runtimeInfo[fields.container_ids]
+            //         if (containers && containers.length > 0) {
+            //             visible = true
+            //         }
+            //     }
+            // }
         }
         return visible
     }
 
-    onTerminal = (action, data) => {
-        if (data.DeploymentType === 'vm') {
-            let execrequest =
-            {
-                app_inst_key:
-                {
-                    app_key:
-                    {
-                        organization:  data[fields.organizationName],
-                        name: data[fields.appName],
-                        version: data[fields.version]
-                    },
-                    cluster_inst_key:
-                    {
-                        cluster_key: { name: data[fields.clusterName] },
-                        cloudlet_key: { organization: data[fields.operatorName], name: data[fields.cloudletName] }
-                    }
-                }
-            }
-
-            let requestedData = {
-                execrequest: execrequest,
-                region: data[fields.region]
-            }
-
-            let store = JSON.parse(localStorage.PROJECT_INIT);
-            let token = store ? store.userToken : 'null';
-            let requestData = {
-                token: token,
-                method: serviceMC.getEP().SHOW_CONSOLE,
-                data: requestedData
-            }
-            this.props.handleLoadingSpinner(true)
-            serviceMC.sendWSRequest(requestData, this.setRemote)
-        }
-        else if (data[fields.runtimeInfo][fields.container_ids]) {
-            this.setState({ terminalData: data, openTerminal: true })
-        }
+    onTerminal = (action, data) => {    
+        this.setState({ terminalData: data, openTerminal: true })
     }
 
     onUpdateVisible = (data)=>
@@ -144,7 +114,7 @@ class AppInstList extends React.Component {
         for (let i = 0; i < this.keys.length; i++) {
             let key = this.keys[i]
             if (key.field === fields.state) {
-                key.customizedData = constant.showProgress
+                key.customizedData = shared.showProgress
             }
             if(key.field === fields.region)
             {
