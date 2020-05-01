703975baf6c252ce8b826be2a03d61fa0792cd40
rahuldsce <rahul.jain@mobiledgex.com>
2020-05-01 10:34:59 +0530

    changed due to do (#346)

--- src/sites/siteFour/appInst/appInstList.js
+++ src/sites/siteFour/appInst/appInstList.js
@@ -75,7 +75,7 @@ class AppInstList extends React.Component {
 
     getDeleteActionMessage = (action, data) => {
         if (data[fields.cloudletStatus] !== constant.CLOUDLET_STATUS_READY && isAdmin()) {
-            return `Cloudlet status is not online, due you still want to proceed with ${data[fields.appName]} App Instance deletion?`
+            return `Cloudlet status is not online, do you still want to proceed with ${data[fields.appName]} App Instance deletion?`
         }
     }
 
