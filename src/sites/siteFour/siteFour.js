7a6c60654e967c9af8e514a44163d70b4c2c0341
rahuldsce <rahul.jain@mobiledgex.com>
2020-05-15 10:34:29 +0530

    EDGECLOUD-2748 WEB UI - Full menu is not loading when logging in as mexadmin (#398)
    * Removed unwanted code from siteFour.js
    
    * Menu loads on login for main

--- src/sites/siteFour/siteFour.js
+++ src/sites/siteFour/siteFour.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import {Header, Button, Icon} from 'semantic-ui-react';
 import sizeMe from 'react-sizeme';
 
 import {withRouter} from 'react-router-dom';
-import {Motion} from "react-motion";
-import {Steps, Hints} from 'intro.js-react';
+import {Steps} from 'intro.js-react';
 
 //redux
 import {connect} from 'react-redux';
 import * as actions from '../../actions';
-
 import {GridLoader} from "react-spinners";
-
 import SideNav from './defaultLayout/sideNav'
-
-
 import * as serviceMC from '../../services/serviceMC';
-
-import {organizationTutor, CloudletTutor, CloudletPoolTutor, PolicyTutor, MonitoringTutor, AuditTutor} from '../../tutorial';
-
+import * as serverData from '../../services/model/serverData';
+import {MonitoringTutor} from '../../tutorial';
 import Alert from 'react-s-alert';
-
 import '../../css/introjs.css';
 import '../../css/introjs-dark.css';
-import changeStep from "../../reducers/changeStep";
 
-let defaultMotion = {left: window.innerWidth / 2, top: window.innerHeight / 2, opacity: 1}
 let _self = null
-
 const monitoringSteps = MonitoringTutor();
-
 
 class SiteFour extends React.Component {
     constructor(props) {
@@ -37,40 +25,24 @@ class SiteFour extends React.Component {
         _self = this
         let store = localStorage.PROJECT_INIT ? JSON.parse(localStorage.PROJECT_INIT) : null
         this.state = {
-            contHeight: 0,
-            contWidth: 0,
-            bodyHeight: 0,
             headerTitle: '',
             activeItem: 'Organizations',
             page: 'pg=0',
             email: store ? store.email : 'Administrator',
             role: '',
             userToken: null,
-            userName: '',
-            regions: [
-                {key: 1, text: 'All', value: 'All'},
-            ],
-            setMotion: defaultMotion,
             OrganizationName: '',
             adminShow: false,
-            searchChangeValue: 'Username',
             menuClick: false,
-            showItem: false,
             learned: false,
             stepsEnabled: false,
             initialStep: 0,
             steps: [],
             enable: false,
             hideNext: true,
-            regionToggle: false,
-            intoCity: false,
-            fullPage: null,
-            menuW: 240,
+            userRole:null,
             selectRole: ''
         };
-
-        this.headerH = 70;
-        this.hgap = 0;
     }
 
     gotoUrl(site, subPath) {
@@ -87,18 +59,13 @@ class SiteFour extends React.Component {
     handleItemClick(id, label, pg, role) {
         localStorage.setItem('selectMenu', label)
         _self.setState({menuClick: true})
-        _self.props.handleChangeViewBtn(false);
-        _self.props.handleChangeClickCity([]);
-        _self.props.handleChangeComputeItem(label);
-        _self.props.handleSearchValue('')
-        _self.props.handleChangeRegion('All')
         _self.props.history.push({
             pathname: '/site4',
             search: "pg=" + pg
         });
         _self.props.history.location.search = "pg=" + pg;
         _self.props.handleChangeStep(pg)
-        _self.setState({page: 'pg=' + pg, activeItem: label, headerTitle: label, intoCity: false})
+        _self.setState({page: 'pg=' + pg, activeItem: label, headerTitle: label})
     }
 
     controllerOptions(option) {
@@ -133,17 +100,19 @@ class SiteFour extends React.Component {
     }
 
 
-    receiveAdminInfo = (mcRequest) => {
+    userRoleInfo = async () => {
+        let mcRequest = await serverData.showUserRoles(this)
         if (mcRequest) {
-            if (mcRequest.response) {
-                let response = mcRequest.response;
-                _self.props.handleRoleInfo(response.data)
-                response.data.map((item, i) => {
-                    if (item.role.indexOf('Admin') > -1) {
-                        _self.setState({adminShow: true});
-                        localStorage.setItem('selectRole', item.role)
+            if (mcRequest.response && mcRequest.response.data) {
+                let dataList = mcRequest.response.data;
+                for (var i = 0; i < dataList.length; i++) {
+                    let role = dataList[i].role
+                    if (role.indexOf('Admin') > -1) {
+                        _self.setState({ userRole: role });
+                        localStorage.setItem('selectRole', role)
+                        break;
                     }
-                })
+                }
             }
         }
     }
@@ -163,15 +132,9 @@ class SiteFour extends React.Component {
             }
         }
         _self.setState({steps: currentStep})
-
-        let elmentName = (_self.steps) ? currentStep : null;
-        //_self.steps.props.options.hideNext = true;
-        let element = (elmentName) ? document.getElementsByClassName(elmentName[0].element.replace('.', '')) : [];
         if (enable) {
-            console.log("elementelement111", element)
             _self.setState({stepsEnabled: true, enable: true})
         }
-
     }
 
     UNSAFE_componentWillMount() {
@@ -181,41 +144,15 @@ class SiteFour extends React.Component {
             headerTitle: (localStorage.selectMenu) ? localStorage.selectMenu : 'Organizations'
         })
 
-
         if (store) {
             _self.getAdminInfo(store.userToken);
         } else {
             _self.gotoUrl('/logout')
         }
-        setTimeout(() => {
-            let elem = document.getElementById('animationWrapper')
-            if (elem) {
-            }
-        }, 4000)
-
-        _self.setState({intoCity: false});
         _self.props.handleViewMode( null );
-        //
-        if (_self.props.params.subPath !== 'pg=audits') {
-            _self.getDataAudit();
-        }
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
-        try {
-            _self.setState({bodyHeight: (window.innerHeight - _self.headerH)})
-            _self.setState({contHeight: (nextProps.size.height - _self.headerH) / 2 - _self.hgap})
-            _self.setState({contWidth: (window.innerWidth - _self.state.menuW)})
-            _self.setState({userToken: nextProps.userToken})
-            _self.setState({userName: (nextProps.userInfo && nextProps.userInfo.info) ? nextProps.userInfo.info.Name : null})
-
-        } catch (e) {
-
-        }
-        if (nextProps.selectedOrg) {
-            _self.setState({selectOrg: nextProps.selectedOrg})
-        }
-
         if (nextProps.params && nextProps.params.subPath) {
             let subPaths = nextProps.params.subPath;
             let subPath = '';
@@ -253,23 +190,9 @@ class SiteFour extends React.Component {
             nextProps.handleAlertInfo('', '');
         }
 
-        //set filters
-        if (nextProps.tableHeaders) {
-            _self.setState({showItem: false})
-            if (nextProps.tableHeaders.length) {
-                _self.setState({tableHeaders: nextProps.tableHeaders})
-            } else {
-
-            }
-        }
-
-        // set step value of guide
-
-        // saved tutorial
         let tutorial = localStorage.getItem('TUTORIAL')
 
         let formKey = Object.keys(nextProps.formInfo);
-        //let submitSucceeded = (nextProps.formInfo) ? nextProps.formInfo[formKey[0]]['submitSucceeded']: null
         if (formKey.length) {
             if (nextProps.formInfo[formKey[0]]['submitSucceeded']) {
                 if (nextProps.formInfo[formKey[0]]['submitSucceeded'] === true) {
@@ -277,10 +200,7 @@ class SiteFour extends React.Component {
                 }
             }
         }
-        if (tutorial === 'done') {
-            //_self.setState({stepsEnabled:false})
-        }
-        //
+
         let enable = true;
         setTimeout(() => {
             if (enable && !_self.state.learned && !tutorial) {
@@ -291,132 +211,19 @@ class SiteFour extends React.Component {
 
         }, 1000)
 
-        let site = _self.props.siteName;
         if (!_self.props.changeStep || _self.props.changeStep === '02') {
             _self.setState({enable: true})
         } else {
             _self.setState({enable: false})
         }
-
-        if (nextProps.regionInfo.region.length && !_self.state.regionToggle) {
-
-            let getRegions = []
-            _self.setState({regionToggle: true})
-            if (nextProps.regionInfo.region) {
-                nextProps.regionInfo.region.map((region, i) => {
-                    getRegions.push({key: i + 2, text: region, value: region})
-                })
-            }
-            let newRegions = Object.assign([], _self.state.regions).concat(getRegions)
-            _self.setState({regions: newRegions})
-
-        }
-        if (nextProps.clickCity.length > 0) {
-            _self.setState({intoCity: true})
-        } else {
-            _self.setState({intoCity: false})
-        }
     }
-
-    componentWillUnmount() {
-        _self.setState({learned: false})
-    }
-
-    searchChange = (e, {value}) => {
-        _self.setState({searchChangeValue: value})
-        _self.props.handleSearchValue(_self.props.searchValue, value)
-    }
-
-
-    options = [
-        {
-            key: 1,
-            text: 'Mobile',
-            value: 1,
-            content: <Header icon='mobile' content='Mobile' subheader='The smallest size'/>,
-        },
-        {
-            key: 2,
-            text: 'Tablet',
-            value: 2,
-            content: <Header icon='tablet' content='Tablet' subheader='The size in the middle'/>,
-        },
-        {
-            key: 3,
-            text: 'Desktop',
-            value: 3,
-            content: <Header icon='desktop' content='Desktop' subheader='The largest size'/>,
-        },
-    ]
 
     onExit() {
         _self.setState({stepsEnabled: false})
     }
 
-
-    /**
-     * audit
-     * */
-    reduceAuditCount(all, data) {
-        let itemArray = [];
-        let addArray = [];
-        let savedArray = localStorage.getItem('auditUnChecked');
-        let checkedArray = localStorage.getItem('auditChecked');
-        let checked = [];
-        all.map((item, i) => {
-            if (savedArray && JSON.parse(savedArray).length) {
-                //이전에 없던 데이터 이면 추가하기
-                if (JSON.parse(savedArray).findIndex(k => k == item.traceid) === -1) addArray.push(item.traceid)
-            } else {
-                itemArray.push(item.traceid)
-            }
-        })
-
-        if (addArray.length) {
-            JSON.parse(savedArray).concat(addArray);
-        }
-
-        let checkResult = null;
-
-        if (savedArray && JSON.parse(savedArray).length) {
-            checkResult = JSON.parse(savedArray);
-        } else if (itemArray.length) {
-            checkResult = itemArray;
-        }
-
-        checked = (checkedArray) ? JSON.parse(checkedArray) : [];
-        _self.props.handleAuditCheckCount(checkResult.length - checked.length)
-        localStorage.setItem('auditUnChecked', JSON.stringify(checkResult))
-    }
-
-    receiveResult = (mcRequest) => {
-        if (mcRequest) {
-            if (mcRequest.response) {
-                let response = mcRequest.response;
-                let request = mcRequest.request;
-                let checked = localStorage.getItem('auditChecked')
-                if (request.method === serviceMC.getEP().SHOW_SELF) {
-                    _self.reduceAuditCount(response.data, checked)
-                }
-            }
-        }
-        _self.props.handleLoadingSpinner(false);
-    }
-
-    getDataAudit = () => {
-        let store = localStorage.PROJECT_INIT ? JSON.parse(localStorage.PROJECT_INIT) : null
-        _self.setState({devData: []})
-        _self.loadCount = 0;
-        serviceMC.sendRequest(this, {
-            token: store.userToken,
-            method: serviceMC.getEP().SHOW_SELF,
-            data: '{}',
-            showMessage: false
-        }, _self.receiveResult)
-    }
-
     render() {
-        const {stepsEnabled, initialStep, hintsEnabled, hints, steps} = _self.state;
+        const {stepsEnabled, initialStep, steps} = _self.state;
         return (
             <div className='view_body'>
                 {steps ?
@@ -429,11 +236,6 @@ class SiteFour extends React.Component {
                         options={{hideNext: false}}
                         ref={steps => (_self.steps = steps)}
                     /> : null}
-                {hints ?
-                    <Hints
-                        enabled={hintsEnabled}
-                        hints={hints}
-                    /> : null}
                 {(_self.props.loadingSpinner == true) ?
                     <div className="loadingBox" style={{zIndex: 9999}}>
                         <GridLoader
@@ -443,12 +245,17 @@ class SiteFour extends React.Component {
                             loading={_self.props.loadingSpinner}
                         />
                     </div> : null}
-                <SideNav onOptionClick={_self.handleItemClick} isShowHeader={this.props.isShowHeader} email={_self.state.email} data={_self.props.userInfo.info} helpClick={_self.enableSteps} gotoUrl={_self.gotoUrl} viewMode={_self.props.ViewMode}/>
-                <Motion defaultStyle={defaultMotion} style={_self.state.setMotion}>
-                    {interpolatingStyle => <div style={interpolatingStyle} id='animationWrapper'></div>}
-                </Motion>
+                <SideNav onOptionClick={_self.handleItemClick} isShowHeader={this.props.isShowHeader} email={_self.state.email} data={_self.props.userInfo.info} helpClick={_self.enableSteps} gotoUrl={_self.gotoUrl} viewMode={_self.props.ViewMode} userRole={this.state.userRole}/>
             </div>
         );
+    }
+
+    componentDidMount() {
+        this.userRoleInfo()
+    }
+
+    componentWillUnmount() {
+        _self.setState({learned: false})
     }
 
 };
@@ -459,100 +266,43 @@ const mapStateToProps = (state) => {
     let formInfo = (state.form) ? state.form : null;
     let submitInfo = (state.submitInfo) ? state.submitInfo : null;
     let regionInfo = (state.regionInfo) ? state.regionInfo : null;
-    let checkedAudit = (state.checkedAudit) ? state.checkedAudit.audit : null;
-    let selectedOrg = (state.selectOrganiz) ? state.selectOrganiz.org : null;
     let ViewMode = (state.ViewMode) ? state.ViewMode.mode : null;
     let changeStep = (state.changeStep) ? state.changeStep.step : null;
 
     return {
         isShowHeader: state.HeaderReducer.isShowHeader,
-        viewBtn: state.btnMnmt ? state.btnMnmt : null,
         userToken: (state.userToken) ? state.userToken : null,
         userInfo: state.userInfo ? state.userInfo : null,
-        userRole: state.showUserRole ? state.showUserRole.role : null,
-        selectOrg: state.selectOrg.org ? state.selectOrg.org : null,
         loadingSpinner: state.loadingSpinner.loading ? state.loadingSpinner.loading : null,
-        injectData: state.injectData ? state.injectData : null,
         alertInfo: {
             mode: state.alertInfo.mode,
             msg: state.alertInfo.msg
         },
-        searchValue: (state.searchValue.search) ? state.searchValue.search : null,
-        changeRegion: (state.changeRegion.region) ? state.changeRegion.region : null,
-        tableHeaders: (state.tableHeader) ? state.tableHeader.headers : null,
-        filters: (state.tableHeader) ? state.tableHeader.filters : null,
-        siteName: (state.siteChanger) ? state.siteChanger.site : null,
         changeStep: changeStep,
         ViewMode: ViewMode,
-        // dataExist: state.dataExist.data,
         tutorState: tutorState,
         formInfo: formInfo,
         submitInfo: submitInfo,
-        regionInfo: regionInfo,
-        audit: checkedAudit,
-        clickCity: state.clickCityList.list,
-        selectedOrg
+        regionInfo: regionInfo
     }
 };
 
 const mapDispatchProps = (dispatch) => {
     return {
-        toggleHeader: (data) => {
-            dispatch(actions.toggleHeader(data))
-        },
         handleChangeSite: (data) => {
             dispatch(actions.changeSite(data))
         },
         handleChangeStep: (data) => {
             dispatch(actions.changeStep(data))
         },
-        handleInjectData: (data) => {
-            dispatch(actions.injectData(data))
-        },
-        handleChangeViewBtn: (data) => {
-            dispatch(actions.btnManagement(data))
-        },
-        handleChangeComputeItem: (data) => {
-            dispatch(actions.computeItem(data))
-        },
-        handleChangeClickCity: (data) => {
-            dispatch(actions.clickCityList(data))
-        },
         handleUserInfo: (data) => {
             dispatch(actions.userInfo(data))
-        },
-        handleSearchValue: (data, value) => {
-            dispatch(actions.searchValue(data, value))
-        },
-        handleChangeRegion: (data) => {
-            dispatch(actions.changeRegion(data))
-        },
-        handleSelectOrg: (data) => {
-            dispatch(actions.selectOrganiz(data))
-        },
-        handleUserRole: (data) => {
-            dispatch(actions.showUserRole(data))
-        },
-        handleComputeRefresh: (data) => {
-            dispatch(actions.computeRefresh(data))
         },
         handleLoadingSpinner: (data) => {
             dispatch(actions.loadingSpinner(data))
         },
-        handleDetail: (data) => {
-            dispatch(actions.changeDetail(data))
-        },
-        handleRoleInfo: (data) => {
-            dispatch(actions.roleInfo(data))
-        },
         handleAlertInfo: (mode, msg) => {
             dispatch(actions.alertInfo(mode, msg))
-        },
-        handleAuditCheckCount: (data) => {
-            dispatch(actions.setCheckedAudit(data))
-        },
-        handleResetMap: (data) => {
-            dispatch(actions.resetMap(data))
         },
         handleViewMode: (data) => {
             dispatch(actions.viewMode(data))
