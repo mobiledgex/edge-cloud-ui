a24537843715e5ec038d13b98be68ed8a1d81fd8
kyungjoongo3 <iamjessica77777@gmail.com>
2020-05-19 08:24:44 +0900

    have arranged some codes.#7777
    Signed-off-by: kyungjoongo3 <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/components/ClientSummaryHooks.js
+++ src/sites/siteFour/monitoring/components/ClientSummaryHooks.js
@@ -54,7 +54,7 @@ export default function ClientSummaryHooks(props) {
                        stickyHeader={true}>
 
 
-                    <TableHead style={{backgroundColor: 'red', fontFamily: 'Roboto', fontSize: 20}} fixedHeader={true}>
+                    <TableHead style={{backgroundColor: 'red', fontFamily: 'Roboto', fontSize: 20}} fixedheader={true}>
                         <TableRow>
                             <TableCell padding={'none'} align="center" style={{}}>
                                 App Inst
