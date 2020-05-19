a103ba2efd7533c5377a48e9a3fc700a497b69be
jiyoonjung <48228788+jiyoonjung@users.noreply.github.com>
2020-05-20 00:02:33 +0900

    Ec 2685 (#412)
    * EDGECLOUD-2685 Web UI - Shared Volume Size is missing from new Cluster Instance and App Instance page
    
    * optimized if condition
    
    Co-authored-by: jiyoonjung <jiyoon.jung@bictech.com>
    Co-authored-by: Rahul.Jain <rahul.jain@mobiledgex.com>

--- src/sites/siteFour/clusterInst/clusterInstReg.js
+++ src/sites/siteFour/clusterInst/clusterInstReg.js
@@ -140,7 +140,7 @@ class ClusterInstReg extends React.Component {
                 form.value = currentForm.value === constant.DEPLOYMENT_TYPE_KUBERNETES ? 1 : undefined
                 form.visible = currentForm.value === constant.DEPLOYMENT_TYPE_DOCKER ? false : true
             }
-            else if (form.field === fields.numberOfNodes) {
+            else if (form.field === fields.numberOfNodes || form.field === fields.sharedVolumeSize) {
                 form.visible = currentForm.value === constant.DEPLOYMENT_TYPE_DOCKER ? false : true
             }
         }
@@ -379,6 +379,7 @@ class ClusterInstReg extends React.Component {
             { field: fields.flavorName, label: 'Flavor', formType: 'Select', placeholder: 'Select Flavor', rules: { required: true }, visible: true, dependentData: [{ index: 1, field: fields.region }] },
             { field: fields.numberOfMasters, label: 'Number of Masters', formType: 'Input', placeholder: 'Enter Number of Masters', rules: { type: 'number', disabled: true }, visible: false, value: 1, update: true },
             { field: fields.numberOfNodes, label: 'Number of Workers', formType: 'Input', placeholder: 'Enter Number of Workers', rules: { type: 'number' }, visible: false, update: true },
+            { field: fields.sharedVolumeSize, label: 'Shared Volume Size', formType: 'Input', placeholder: 'Enter Shared Volume Size', unit: 'GB', rules: { type: 'number' }, visible: false, update: true },
             { field: fields.reservable, label: 'Reservable', formType: 'Checkbox', visible: true, roles: ['AdminManager'], value: false, update: true },
         ]
     }
