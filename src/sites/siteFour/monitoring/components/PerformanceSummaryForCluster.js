210ac57532ff2f21a5fce156145b4baef687e773
kyungjoon.go2 <iamjessica77777@gmail.com>
2020-06-01 09:00:02 +0900

    code clean up
    Signed-off-by: kyungjoon.go2 <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/components/PerformanceSummaryForCluster.js
+++ src/sites/siteFour/monitoring/components/PerformanceSummaryForCluster.js
@@ -83,7 +83,7 @@ export default function PerformanceSummaryForCluster(props: Props) {
                     <Table size="small" aria-label="a dense table " style={{width: '100%', overflowX: 'scroll',}}
                            stickyheader={true}>
 
-                        <TableHead style={{backgroundColor: 'red', fontFamily: 'Roboto', fontSize: 20}} fixedheader={true.toString()}>
+                        <TableHead style={{backgroundColor: '#303030', fontFamily: 'Roboto', fontSize: 20}} fixedheader={true.toString()}>
                             <TableRow>
                                 <TableCell padding={'none'} align="center" style={{}}>
                                 </TableCell>
