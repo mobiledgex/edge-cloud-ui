38d13d3ac26f117401621aa28a175eb8a583d19e
Rahul.Jain <rahul.jain@mobiledgex.com>
2020-06-06 11:47:25 +0530

    Alert dialog note added

--- src/hoc/dialog/mexWarningDialog.js
+++ src/hoc/dialog/mexWarningDialog.js
@@ -28,9 +28,10 @@ const MexMessageDialog = (props) => {
                     <Typography style={{ color: '#FFF' }}>
                         {message}
                     </Typography>
-                    <Typography style={{ color: '#FF5252', marginTop:20, fontSize:12 }}>
-                        {props.messageInfo.action.dialogNote ? props.messageInfo.action.dialogNote : null}
-                    </Typography>
+                    {props.messageInfo.action.dialogNote ?
+                        <Typography style={{ color: '#FF5252', marginTop: 20, fontSize: 12 }}>
+                            {props.messageInfo.action.dialogNote}
+                        </Typography> : null}
                 </DialogContent>
                 <DialogActions style={{ background: '#616161' }}>
                     <Button onClick={() => { props.onClick(false) }} style={{color:'#D3D3D3'}}>
