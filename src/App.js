8283c70126ef7571e74b6f1cf9a22ca0781b62d0
Rahul.Jain <rahul.jain@mobiledgex.com>
2020-06-10 17:19:52 +0530

    Code Splitting Implemented for App.js modules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import {HashRouter, Route } from "react-router-dom";
 
 import 'semantic-ui-css/semantic.min.css';
@@ -9,9 +9,6 @@ import * as serverData from './services/model/serverData';
 import { LOCAL_STRAGE_KEY } from './components/utils/Settings'
 //insert pages
 import './app.css';
-import EntranceGlob from './sites/login/entranceGlob';
-import VerifyContent from './container/verifyContent';
-import SiteFour from './sites/siteFour/siteFour'
 import './css/index.css';
 import './css/pages/audit.css';
 import './css/pages/cloudletPool.css';
@@ -19,6 +16,11 @@ import './css/pages/monitoring.css';
 import './css/components/timelineH.css';
 import { ThemeProvider } from "@material-ui/styles";
 import { getDarkTheme, getLightTheme, THEME_TYPE } from "./themeStyle";
+import { GridLoader } from 'react-spinners';
+
+const EntranceGlob = lazy(() => import('./sites/login/entranceGlob'));
+const VerifyContent = lazy(() => import('./container/verifyContent'));
+const SiteFour = lazy(() => import('./sites/siteFour/siteFour'))
 
 let self = null;
 class App extends Component {
@@ -57,18 +59,33 @@ class App extends Component {
         this.getControllers()
     }
 
+    loader = () => {
+        return (
+            <div>
+                <GridLoader
+                    sizeUnit={"px"}
+                    size={20}
+                    color={'#70b2bc'}
+                    loading={true}
+                />
+            </div>
+        )
+    }
+
     render() {
         return (
             <ThemeProvider theme={this.props.themeType === THEME_TYPE.DARK ? getDarkTheme() : getLightTheme()}>
                 <HashRouter>
-                    <div style={{ width: '100%', height: '100%' }}>
-                        <Route exact path='/' component={EntranceGlob} />
-                        <Route exact path='/site4' component={SiteFour} />
-                        <Route exact path='/site4/:pageId' component={SiteFour} />
-                        <Route exact path='/logout' component={EntranceGlob}/>
-                        <Route exact path='/passwordreset' component={EntranceGlob}/>
-                        <Route exact path='/verify' component={VerifyContent}/>
-                    </div>
+                    <Suspense fallback={this.loader}>
+                        <div style={{ width: '100%', height: '100%' }}>
+                            <Route exact path='/' component={EntranceGlob} />
+                            <Route exact path='/site4' component={SiteFour} />
+                            <Route exact path='/site4/:pageId' component={SiteFour} />
+                            <Route exact path='/logout' component={EntranceGlob} />
+                            <Route exact path='/passwordreset' component={EntranceGlob} />
+                            <Route exact path='/verify' component={VerifyContent} />
+                        </div>
+                    </Suspense>
                 </HashRouter>
             </ThemeProvider>
 
