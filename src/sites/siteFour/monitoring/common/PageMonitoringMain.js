dc788a5e77bd08eddd696e2799997fa3a785cdc2
KO_jessica <iamjessica77777@gmail.com>
2020-06-20 21:18:21 +0900

    Code refactoring.777777
    Signed-off-by: KO_jessica <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/common/PageMonitoringMain.js
+++ src/sites/siteFour/monitoring/common/PageMonitoringMain.js
@@ -8,6 +8,16 @@ export default function PageMonitoringMain() {
 
     const renderMainPage = () => {
         try {
+            /*if (userRole.includes('Admin')) {
+                return (
+                    <PageMonitoringForAdmin/>
+                )
+            } else {
+                return (
+
+                )
+            }*/
+
             return <PageDevOperMonitoringView/>
         } catch (e) {
 
