fa29d39c162c10fd00d569f4a0aa7611824251fe
KO_jessica <iamjessica77777@gmail.com>
2020-07-06 19:30:10 +0900

    code clean up
    Signed-off-by: KO_jessica <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/common/PageMonitoringMain.js
+++ src/sites/siteFour/monitoring/common/PageMonitoringMain.js
@@ -3,12 +3,9 @@ import PageMonitoringView from "../view/PageMonitoringView";
 import {Card} from "@material-ui/core";
 
 export default function PageMonitoringMain() {
-    const renderMainPage = () => {
-        return <PageMonitoringView/>
-    }
     return (
         <Card style={{width: '100%', height: '100%', backgroundColor: '#292c33', padding: 10, color: 'white'}}>
-            {renderMainPage()}
+            <PageMonitoringView/>
         </Card>
     );
 }
