4ffaf573eaa508c3b360532cb4dfc448858ea7ec
KO_jessica <iamjessica77777@gmail.com>
2020-07-09 05:50:02 +0900

    code clean up
    Signed-off-by: KO_jessica <iamjessica77777@gmail.com>

--- src/sites/siteFour/monitoring/common/PageMonitoringMain.js
+++ src/sites/siteFour/monitoring/common/PageMonitoringMain.js
@@ -1,11 +1,16 @@
 import React from "react";
 import PageMonitoringView from "../view/PageMonitoringView";
 import {Card} from "@material-ui/core";
+import {showToast} from "../service/PageMonitoringCommonService";
 
 export default function PageMonitoringMain() {
-    return (
-        <Card style={{width: '100%', height: '100%', backgroundColor: '#292c33', padding: 10, color: 'white'}}>
-            <PageMonitoringView/>
-        </Card>
-    );
+    try {
+        return (
+            <Card style={{width: '100%', height: '100%', backgroundColor: '#292c33', padding: 10, color: 'white'}}>
+                <PageMonitoringView/>
+            </Card>
+        );
+    } catch (e) {
+       // showToast(e.toString())
+    }
 }
