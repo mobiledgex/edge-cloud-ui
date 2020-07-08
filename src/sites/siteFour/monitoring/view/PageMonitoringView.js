6c6adb0fd2af64933d2b43ebf15f88e0b2d6d0ed
Rahul.Jain <rahul.jain@mobiledgex.com>
2020-07-08 17:12:02 +0530

    org cluster  width

--- src/sites/siteFour/monitoring/view/PageMonitoringView.js
+++ src/sites/siteFour/monitoring/view/PageMonitoringView.js
@@ -2529,7 +2529,7 @@ export default withSize()(connect(PageDevMonitoringMapStateToProps, PageDevMonit
                             dropdownMatchSelectWidth={false}
                             showSearch={true}
                             value={this.state.currentOrg}
-                            style={{width: 150, maxHeight: '512px !important', fontSize: '9px !important'}}
+                            style={{width: 100, maxHeight: '512px !important', fontSize: '9px !important'}}
                             dropdownStyle={{
                                 maxHeight: 800, overflow: 'auto', width: '250px'
                             }}
@@ -2785,7 +2785,7 @@ export default withSize()(connect(PageDevMonitoringMapStateToProps, PageDevMonit
                             <TreeSelect
                                 dropdownMatchSelectWidth={false}
                                 dropdownStyle={{
-                                    maxHeight: 800, overflow: 'auto', width: '520px'
+                                    maxHeight: 800, overflow: 'auto', width: '420px'
                                 }}
                                 showArrow={true}
                                 maxTagCount={maxTagCount}
