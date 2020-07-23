760228dea6a079da82f228ac9a7c7aaae2364b7f
kyungjoongo2 <58411936+kyungjoongo2@users.noreply.github.com>
2020-07-23 15:26:20 +0900

    have Adjusted the header size of add item popup. (#533)
    have Adjusted the header size of add item popup.

--- src/sites/siteFour/monitoring/components/AddItemPopupContainerNew.js
+++ src/sites/siteFour/monitoring/components/AddItemPopupContainerNew.js
@@ -406,7 +406,7 @@ export default class AddItemPopupContainerNew extends React.Component<Props, Sta
                             HW Type
                         </Center>
                     </div>
-                    <div className='page_monitoring_form_column_right'>
+                    <div className='page_monitoring_form_column_right2'>
                         <Select
                             maxTagCount={4}
                             allowClear={true}
@@ -450,6 +450,37 @@ export default class AddItemPopupContainerNew extends React.Component<Props, Sta
 
     }
 
+    reanderPopupHeader() {
+        return (
+            <div className='add_item_popup_outer_header'>
+                <div style={{fontSize: 14, marginLeft: 0, flex: .95, marginTop: 5, fontWeight: 'bold'}}>
+                    Add Item [{convertToClassification(this.props.parent.state.currentClassification)}]
+                </div>
+                <div className='add_item_popup_outer_header_right_icon'>
+                    <div className='headerIconDiv' style={{flex: .5, cursor: 'pointer'}} onClick={this.handleAddClicked}>
+                        <MaterialIcon
+                            size={'small'}
+                            icon='check'
+                            color={'#77BD25'}
+                        />
+                    </div>
+                    <div style={{width: 10,}}/>
+                    <div className='headerIconDiv' onClick={async () => {
+                        this.closePopupWindow();
+                    }}>
+                        <MaterialIcon
+                            size={'small'}
+                            icon='close'
+                            color={'#77BD25'}
+                        />
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
+
     render() {
 
         let hardwareDropdownList = this.makeHwDropdownList();
@@ -458,70 +489,46 @@ export default class AddItemPopupContainerNew extends React.Component<Props, Sta
         return (
             <div className='add_item_popup_outer'>
                 <div style={{width: '100%', zIndex: 999999}}>
-                    <div className='add_item_popup_outer_header'>
-                        <div style={{fontSize: 14, marginLeft: -5, flex: .95, marginTop: 15, fontWeight: 'bold'}}>
-                            Add Item [{convertToClassification(this.props.parent.state.currentClassification)}]
-                        </div>
-                        <div className='add_item_popup_outer_header_right_icon'>
-                            <div style={{flex: .5, cursor: 'pointer'}} onClick={this.handleAddClicked}>
-                                <MaterialIcon
-                                    size={'tiny'}
-                                    icon='check'
-                                    color={'#77BD25'}
 
-
-                                />
-                            </div>
-                            <div style={{width: 10,}}/>
-                            <div style={{flex: .5, cursor: 'pointer'}} onClick={async () => {
-                                this.closePopupWindow();
-                            }}>
-                                <MaterialIcon
-                                    size={'tiny'}
-                                    icon='close'
-                                    color={'#77BD25'}
-
-                                />
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className='page_monitoring_popup_title_divide'/>
+                    {this.reanderPopupHeader()}
+                    <div className='page_monitoring_popup_title_divide' style={{}}/>
                     <div className='page_monitoring_form_row'>
                         {/*todo:############################*/}
                         {/*@todo:ITEM TYPE                  */}
                         {/*todo:############################*/}
-                        <div style={{flex: .25, display: 'flex'}}>
+                        <div style={{flex: .5, display: 'flex'}}>
                             <div className='page_monitoring_form_column_left'>
-                                <Center>
+                                <Center style={{marginLeft: 25,}}>
                                     Item Type
                                 </Center>
                             </div>
-                            <Radio.Group
-                                onChange={(e) => {
-                                    let selectedItem = e.target.value;
-                                    this.setState({
-                                        currentItemType: selectedItem,
-                                    });
-                                }}
-                                value={this.state.currentItemType}
-                            >
-                                <div className='page_monitoring_form_column_right'>
-                                    {this.renderLineChartRadio()}
-                                    {this.renderBarChartRadio()}
-                                    {this.renderColumnChartRadio()}
-                                    {this.renderTableRadio()}
-                                    {this.renderMapRadio()}
-                                    {this.props.parent.state.currentClassification === CLASSIFICATION.CLUSTER
-                                    || this.props.parent.state.currentClassification === CLASSIFICATION.APP_INST_FOR_ADMIN
-                                    || this.props.parent.state.currentClassification === CLASSIFICATION.APPINST ? this.renderBubbleRadio() : null}
-                                </div>
-                            </Radio.Group>
+                            <div style={{marginLeft: 25,}}>
+                                <Radio.Group
+                                    onChange={(e) => {
+                                        let selectedItem = e.target.value;
+                                        this.setState({
+                                            currentItemType: selectedItem,
+                                        });
+                                    }}
+                                    value={this.state.currentItemType}
+                                >
+                                    <div className='page_monitoring_form_column_right'>
+                                        {this.renderLineChartRadio()}
+                                        {this.renderBarChartRadio()}
+                                        {this.renderColumnChartRadio()}
+                                        {this.renderTableRadio()}
+                                        {this.renderMapRadio()}
+                                        {this.props.parent.state.currentClassification === CLASSIFICATION.CLUSTER
+                                        || this.props.parent.state.currentClassification === CLASSIFICATION.APP_INST_FOR_ADMIN
+                                        || this.props.parent.state.currentClassification === CLASSIFICATION.APPINST ? this.renderBubbleRadio() : null}
+                                    </div>
+                                </Radio.Group>
+                            </div>
                         </div>
                         {/*todo:############################*/}
                         {/*@todo:HW TYPE                    */}
                         {/*todo:############################*/}
-                        <div className='page_monitoring_hw_type_div'>
+                        <div className='page_monitoring_hw_type_div' style={{flex: .5}}>
                             {/*@todo:MultipleDropdown           */}
                             {this.state.isShowHWDropDown && this.renderHwMultipleDropdown(hwDropdownChildren)}
                             {/*@todo:TABLE TYPE             */}
