3c1660fef0491ba5f1dfd75af3bda11982ed81c3
Rahul.Jain <rahul.jain@mobiledgex.com>
2020-08-09 10:24:47 +0530

    Flavor was missing while creating appinst

--- src/services/model/appInstance.js
+++ src/services/model/appInstance.js
@@ -32,7 +32,7 @@ export const keys = () => ([
   { field: fields.createdAt, serverField: 'created_at', label: 'Created', dataType: constant.TYPE_DATE, date: { format: FORMAT_FULL_DATE_TIME, dataFormat: 'seconds' } },
   { field: fields.status, serverField: 'status', label: 'Status', dataType: constant.TYPE_JSON },
   { field: fields.configs, serverField: 'configs', label: 'Configs', dataType: constant.TYPE_JSON },
-  //{ field: fields.healthCheck, serverField: 'health_check', label: 'Health Status', visible: false},  
+  { field: fields.healthCheck, serverField: 'health_check', label: 'Health Status', visible: true},  
   { field: fields.autoPolicyName, label: 'Auto Prov Policy', visible: false},  
   { field: fields.actions, label: 'Actions', sortable: false, visible: true, clickable: true }
 ])
@@ -63,6 +63,10 @@ export const getKey = (data, isCreate) => {
 
     if (data[fields.sharedVolumeSize]) {
         appinst.shared_volume_size = parseInt(data[fields.sharedVolumeSize])
+    }
+
+    if (data[fields.flavorName]) {
+      appinst.flavor = {name : data[fields.flavorName]}
     }
 
   }
