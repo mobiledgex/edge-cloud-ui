5c830ae35b2b1f6b9cdb9d87354f6f5ebb203b9b
Rahul.Jain <rahul.jain@mobiledgex.com>
2020-08-10 17:44:39 +0530

    Changes Show Log Height

--- src/container/TerminalViewer.js
+++ src/container/TerminalViewer.js
@@ -12,8 +12,8 @@ import MexForms from '../hoc/forms/MexForms';
 import {fields} from '../services/model/format'
 import { getUserRole } from '../services/model/format';
 import {RUN_COMMAND, SHOW_LOGS, DEVELOPER_VIEWER, DEPLOYMENT_TYPE_VM} from '../constant'
-const Terminal = lazy(() => import('../hoc/terminal/MexTerminal'))
 import '../hoc/terminal/style.css'
+const Terminal = lazy(() => import('../hoc/terminal/MexTerminal'))
 
 
 class MexTerminal extends Component {
