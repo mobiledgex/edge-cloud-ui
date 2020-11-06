a329ce44408eddcc46d05569cd2eee6475628010
rahuldsce <rahul.jain@mobiledgex.com>
2020-11-06 16:41:13 +0530

    Cluster and Cloudlet Map (#666)

--- src/sites/siteFour/monitoringReload/list/EventList.js
+++ src/sites/siteFour/monitoringReload/list/EventList.js
@@ -5,7 +5,6 @@ import './style.css'
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const filters = ['app', 'cluster', 'cloudlet', 'cloudletorg', 'clusterorg', 'apporg']
 class Events extends React.Component {
     constructor(props) {
         super(props)
@@ -13,16 +12,6 @@ class Events extends React.Component {
             expand: -1
         }
     }
-
-    keys = [
-        { serverField: 'mtags' },
-        { serverField: 'name' },
-        { serverField: 'org' },
-        { serverField: 'type' },
-        { serverField: 'region' },
-        { serverField: 'timestamp' },
-        { serverField: 'type' },
-    ]
 
     expandMenu = (id) => {
         this.setState(prevState => ({ expand: prevState.expand === id ? -1 : id }))
@@ -32,29 +21,26 @@ class Events extends React.Component {
     searchFilterValid = (mtags, filter)=>{
         let search  = filter.search
         let valid = []
-        filters.map(filter => {
-            if (mtags[filter]) {
-                valid.push(mtags[filter].toLowerCase().includes(search.toLowerCase()))
+        this.props.keys.map(key => {
+            if (key.filter && mtags[key.serverField]) {
+                valid.push(mtags[key.serverField].toLowerCase().includes(search.toLowerCase()))
             }
         })
         return valid.includes(true)
     }
 
     render() {
-        const { header, eventData, filter, colors} = this.props
+        const { header, eventData, filter, colors, keys} = this.props
         const { expand } = this.state
         return (
             <div className="event-list-main">
                 <div align="left" className="event-list-header">
-                    <h3>{header}</h3>
+                    <h3>Events</h3>
                 </div>
                 <div style={{ height: 'calc(33vh - 0px)', overflow: 'auto' }}>
                     <List dense={false} >
                         {eventData.map((data, i) => {
                             let mtags = data['mtags']
-                            let cluster = mtags['cluster']
-                            let cloudlet = mtags['cloudlet']
-                            let cloudletorg = mtags['cloudletorg']
                             return (
                                 this.searchFilterValid(mtags, filter) ? <React.Fragment key={i}>
                                     <ListItem key={i} onClick={() => this.expandMenu(i)}>
@@ -63,22 +49,20 @@ class Events extends React.Component {
                                         </ListItemIcon>
                                         <ListItemText id="switch-list-label-wifi" primary={data['name']} secondary={
                                             <React.Fragment>
-                                                {`${mtags['app']} [${mtags['appver']}]`}
-                                                {cluster ? <code style={{color:'#74B724'}}><br/>{`${cluster}`}</code> : null}
+                                                {this.props.header(mtags)}
                                                 <br/>
                                                 {dateUtil.time(dateUtil.FORMAT_FULL_DATE_TIME, data['timestamp'])}
                                             </React.Fragment>}  />
                                         {expand === i ? <ExpandLessIcon/> : <ExpandMoreIcon/>}
                                     </ListItem>
                                     <Collapse in={expand === i} timeout="auto" unmountOnExit>
-                                        <List component="div" style={{ marginLeft: 0 }} dense={true}>
-                                            {cloudlet ? <ListItem><ListItemText primary={`Cloudlet: ${cloudlet}`} /></ListItem> :null}
-                                            <ListItem><ListItemText primary={`Cloudlet: ${cloudletorg}`} /></ListItem>
-                                            <ListItem><ListItemText primary={`Hostname: ${mtags['hostname']}`} /></ListItem>
-                                            <ListItem><ListItemText primary={`Line no: ${mtags['lineno']}`} /></ListItem>
-                                            <ListItem> <ListItemText primary={`Span ID: ${mtags['spanid']}`} /></ListItem>
-                                            <ListItem> <ListItemText primary={`State: ${mtags['state']}`} /></ListItem>
-                                            <ListItem> <ListItemText primary={`Trace ID: ${mtags['traceid']}`} /></ListItem>
+                                        <List component="div" style={{ marginLeft: 60 }} dense={true}>
+                                            {keys.map((key, j) => {
+                                                let value = mtags[key.serverField]
+                                                if (key.summary && value) {
+                                                    return <ListItem key={j}><ListItemText primary={`${key.label}: ${value}`} /></ListItem>
+                                                }
+                                            })}
                                         </List>
                                     </Collapse>
                                     <Divider component="li" />
