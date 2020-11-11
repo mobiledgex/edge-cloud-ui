1edeb40805f8a9ab8c63e6fd4bf8da6e0e0a9d90
Rahul.Jain <toniqapps@gmail.com>
2020-11-11 19:26:55 +0530

    Show Alert Implemented

--- src/services/model/alerts.js
+++ src/services/model/alerts.js
@@ -9,26 +9,26 @@ import { ADMIN_MANAGER, RECEIVER_TYPE_SLACK, RECEIVER_TYPE_EMAIL } from '../../c
 import {appInstanceKey} from './appInstClient'
 
 let fields = formatter.fields
-const showAlertKeys = () => (
+export const showAlertKeys = () => (
     [
-        { field: fields.region, label: 'Region', sortable: true, visible: true, filter: true },
-        { field: fields.alertname, serverField: 'labels#OS#alertname', label: 'Alert Name', sortable: true, visible: true },
-        { field: fields.appName, serverField: 'labels#OS#app', label: 'App', sortable: true, visible: true },
-        { field: fields.appDeveloper, serverField: 'labels#OS#apporg', label: 'App Developer', sortable: true, visible: true },
-        { field: fields.appDeveloper, serverField: 'labels#OS#apporg', label: 'App Developer', sortable: true, visible: true },
-        { field: fields.version, serverField: 'labels#OS#appver', label: 'App Version', sortable: true, visible: true },
-        { field: fields.cloudletName, serverField: 'labels#OS#cloudlet', label: 'Cloudlet', sortable: true, visible: true },
-        { field: fields.operatorName, serverField: 'labels#OS#cloudletorg', label: 'Operator', sortable: true, visible: true },
-        { field: fields.clusterName, serverField: 'labels#OS#cluster', label: 'Cluster', sortable: true, visible: true },
-        { field: fields.clusterdeveloper, serverField: 'labels#OS#clusterorg', label: 'Cluster Developer', sortable: true, visible: true },
-        { field: fields.envoyclustername, serverField: 'labels#OS#envoy_cluster_name', label: 'Envoy Cluster', sortable: true, visible: true },
-        { field: fields.instance, serverField: 'labels#OS#instance', label: 'Instance', sortable: true, visible: true },
-        { field: fields.job, serverField: 'labels#OS#job', label: 'Job', sortable: true, visible: true },
-        { field: fields.status, serverField: 'labels#OS#status', label: 'Status', sortable: true, visible: true },
-        { field: fields.state, serverField: 'state', label: 'State', sortable: true, visible: true },
-        { field: fields.activeAt, serverField: 'active_at', label: 'Active At', sortable: true, visible: true },
-        { field: fields.notifyId, serverField: 'notify_id', label: 'Notify ID', sortable: true, visible: true },
-        { field: fields.controller, serverField: 'controller', label: 'Controller', sortable: true, visible: true }
+        { field: fields.region, label: 'Region', sortable: true, visible: false, summary: false, filter: false },
+        { field: fields.alertname, serverField: 'labels#OS#alertname', label: 'Alert Name', sortable: true, visible: true, summary: false },
+        { field: fields.appName, serverField: 'labels#OS#app', label: 'App', sortable: true, visible: false, summary: true },
+        { field: fields.appDeveloper, serverField: 'labels#OS#apporg', label: 'App Developer', sortable: true, visible: false, summary: true },
+        { field: fields.appDeveloper, serverField: 'labels#OS#apporg', label: 'App Developer', sortable: true, visible: false, summary: true },
+        { field: fields.version, serverField: 'labels#OS#appver', label: 'App Version', sortable: true, visible: false, summary: true },
+        { field: fields.cloudletName, serverField: 'labels#OS#cloudlet', label: 'Cloudlet', sortable: true, visible: false, summary: true },
+        { field: fields.operatorName, serverField: 'labels#OS#cloudletorg', label: 'Operator', sortable: true, visible: false, summary: true },
+        { field: fields.clusterName, serverField: 'labels#OS#cluster', label: 'Cluster', sortable: true, visible: false, summary: true },
+        { field: fields.clusterdeveloper, serverField: 'labels#OS#clusterorg', label: 'Cluster Developer', sortable: true, visible: false, summary: true },
+        { field: fields.envoyclustername, serverField: 'labels#OS#envoy_cluster_name', label: 'Envoy Cluster', sortable: true, visible: false, summary: true },
+        { field: fields.instance, serverField: 'labels#OS#instance', label: 'Instance', sortable: true, visible: false, summary: true },
+        { field: fields.job, serverField: 'labels#OS#job', label: 'Job', sortable: true, visible: false, summary: true },
+        { field: fields.status, serverField: 'labels#OS#status', label: 'Status', sortable: true, visible: false, summary: true },
+        { field: fields.state, serverField: 'state', label: 'State', sortable: true, visible: false, summary: true },
+        { field: fields.activeAt, serverField: 'active_at', label: 'Active At', sortable: true, visible: false, summary: true },
+        { field: fields.notifyId, serverField: 'notify_id', label: 'Notify ID', sortable: true, visible: false, summary: true },
+        { field: fields.controller, serverField: 'controller', label: 'Controller', sortable: true, visible: false, summary: true }
     ]
 )
 
--- src/sites/siteFour/alerts/AlertLocal.js
+++ src/sites/siteFour/alerts/AlertLocal.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { IconButton, Typography } from '@material-ui/core'
+import { Collapse, Divider, IconButton, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core'
 import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
 import { PAGE_ALERTS } from '../../../constant';
+import {showAlertKeys} from '../../../services/model/alerts'
 import * as actions from '../../../actions';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 class AlertLocal extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            expand: -1
+        }
     }
 
     renderAlertPreferences = () => {
@@ -27,10 +33,62 @@ class AlertLocal extends React.Component {
         </div>
     )
 
+    expandMenu = (id) => {
+        this.setState(prevState => ({ expand: prevState.expand === id ? -1 : id }))
+    }
+
+    header = (data) => {
+        let alertName = data['alertname']
+        return ( 
+            <React.Fragment>
+                {showAlertKeys().map(key=>{
+                    return key.visible ? data[key.field] : null
+                })}
+            </React.Fragment>
+        )
+    }
+
+    renderList = () => {
+        const { data } = this.props
+        const { expand } = this.state
+        return (
+            <List dense={false} >
+                {data.map((item, i) => {
+                    return (
+                        <React.Fragment key={i}>
+                            <ListItem key={i} onClick={() => this.expandMenu(i)}>
+                                {/* <ListItemIcon>
+                                    <div style={{ width: 30, height: 30, borderRadius: 50, backgroundColor: colors[i], color: 'white', textAlign: 'center', padding: 6, fontWeight: 900 }}><b>{data['name'].charAt(0).toUpperCase()}</b></div>
+                                </ListItemIcon> */}
+                                <ListItemText id="switch-list-label-wifi" primary={data['name']} secondary={
+                                    <React.Fragment>
+                                        {this.header(item)}
+                                    </React.Fragment>} />
+                                {expand === i ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                            </ListItem>
+                            <Collapse in={expand === i} timeout="auto" unmountOnExit>
+                                <List component="div" dense={true}>
+                                    {showAlertKeys().map((key, j) => {
+                                        let value = item[key.field]
+                                        if (key.summary && value) {
+                                            return <ListItem key={j}><ListItemText primary={`${key.label}: ${value}`} /></ListItem>
+                                        }
+                                    })}
+                                </List>
+                            </Collapse>
+                            <Divider component="li" />
+                        </React.Fragment>)
+                })}
+            </List>
+        )
+    }
+
     render() {
         return (
             <React.Fragment>
                 {this.renderToolbar()}
+                <br/>
+                {this.renderList()}
             </React.Fragment>
         )
     }
