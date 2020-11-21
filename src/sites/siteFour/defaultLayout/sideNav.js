6a0d02fc37d40ee82a771f5042a834b5b1ab495b
rahuldsce <rahul.jain@mobiledgex.com>
2020-11-21 14:19:45 +0530

    Developer Operator Supports ALert Recevier (#685)

--- src/sites/siteFour/defaultLayout/sideNav.js
+++ src/sites/siteFour/defaultLayout/sideNav.js
@@ -157,7 +157,7 @@ const options = [
     },
     { label: 'Monitoring', icon: <TvOutlinedIcon />, pg: 'Monitoring', pageId: constant.PAGE_MONITORING, page: <PageMonitoringMain />, roles: ['AdminManager', 'DeveloperManager', 'OperatorManager'] },
     { label: 'Reload', icon: <TvOutlinedIcon />, pg: 'Reload', pageId: constant.PAGE_MONITORING_RELOAD, page: <Monitoring />, roles: [constant.DEVELOPER_MANAGER, constant.OPERATOR_MANAGER] },
-    { label: 'Alert Receivers', icon: <NotificationsNoneOutlined />, pg: 'AlertReceivers', pageId: constant.PAGE_ALERTS, page: <Alerts />, roles: [constant.ADMIN_MANAGER] }
+    { label: 'Alert Receivers', icon: <NotificationsNoneOutlined />, pg: 'AlertReceivers', pageId: constant.PAGE_ALERTS, page: <Alerts />, roles: [constant.ADMIN_MANAGER, constant.DEVELOPER_MANAGER, constant.OPERATOR_MANAGER] }
 ]
 
 class SideNav extends React.Component {
