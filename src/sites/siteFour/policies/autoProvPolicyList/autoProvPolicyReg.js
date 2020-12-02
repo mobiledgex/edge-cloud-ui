5cccb9d6a661c5e013f08e4bf7b7530eb4c1b67c
rahuldsce <rahul.jain@mobiledgex.com>
2020-12-03 03:34:33 +0530

    Label changed (#705)

--- src/sites/siteFour/policies/autoProvPolicyList/autoProvPolicyReg.js
+++ src/sites/siteFour/policies/autoProvPolicyList/autoProvPolicyReg.js
@@ -78,12 +78,42 @@ class AutoProvPolicyReg extends React.Component {
         }
     }
 
+    minActiveInstancesValueChange = (currentForm, forms, isInit) => {
+        let currentValue = currentForm.value
+        for (let i = 0; i < forms.length; i++) {
+            let form = forms[i]
+            if (form.field === fields.deployClientCount) {
+                form.rules.required = !(currentValue && currentValue.length > 0)
+                break;
+            }
+        }
+        this.setState({ forms: forms })
+    }
+
+    deployClientCountValueChange = (currentForm, forms, isInit) => {
+        let currentValue = currentForm.value
+        for (let i = 0; i < forms.length; i++) {
+            let form = forms[i]
+            if (form.field === fields.minActiveInstances) {
+                form.rules.required = !(currentValue && currentValue.length > 0)
+                break;
+            }
+        }
+        this.setState({ forms: forms })
+    }
+
     checkForms = (form, forms, isInit, data) => {
         if (form.field === fields.region) {
             this.regionValueChange(form, forms, isInit)
         }
         else if (form.field === fields.organizationName) {
             this.organizationValueChange(form, forms, isInit)
+        }
+        else if (form.field === fields.deployClientCount) {
+            this.deployClientCountValueChange(form, forms, isInit)
+        }
+        else if (form.field === fields.minActiveInstances) {
+            this.minActiveInstancesValueChange(form, forms, isInit)
         }
     }
 
@@ -337,12 +367,12 @@ class AutoProvPolicyReg extends React.Component {
             { field: fields.region, label: 'Region', formType: 'Select', placeholder: 'Select Region', rules: { required: true }, visible: true },
             { field: fields.organizationName, label: 'Organization', formType: 'Select', placeholder: 'Select Organization', rules: { required: getOrganization() ? false : true, disabled: getOrganization() ? true : false }, value: getOrganization(), visible: true, tip: 'Name of the organization for the cluster that this policy will apply to' },
             { field: fields.autoPolicyName, label: 'Auto Policy Name', formType: 'Input', placeholder: 'Enter Auto Provisioning Policy Name', rules: { required: true }, visible: true, tip: 'Policy name' },
-            { field: fields.deployClientCount, label: 'Deploy Request Count', formType: 'Input', rules: { type: 'number', required: false }, visible: true, update: true, dataValidateFunc: this.validatedeployClientCount, updateId: ['3'], tip: 'Minimum number of clients within the auto deploy interval to trigger deployment' },
+            { field: fields.deployClientCount, label: 'Deploy Request Count', formType: 'Input', rules: { type: 'number', required: true, onBlur:true, requiredMsg:'Either Deploy Request Count or Min Active Instances is mandatory' }, visible: true, update: true, dataValidateFunc: this.validatedeployClientCount, updateId: ['3'], tip: 'Minimum number of clients within the auto deploy interval to trigger deployment' },
             { field: fields.undeployClientCount, label: 'Undeploy Request Count', formType: 'Input', rules: { type: 'number', required: false }, visible: true, update: true, updateId: ['8'], tip: 'Number of active clients for the undeploy interval below which trigers undeployment, 0 (default) disables auto undeploy' },
             { field: fields.deployIntervalCount, label: 'Deploy Interval Count (s)', formType: 'Input', rules: { type: 'number' }, visible: true, update: true, updateId: ['4'], tip: 'Number of intervals to check before triggering deployment' },
             { field: fields.undeployIntervalCount, label: 'Undeploy Interval Count (s)', formType: 'Input', rules: { type: 'number' }, visible: true, update: true, updateId: ['9'], tip: 'Number of intervals to check before triggering undeployment' },
             { field: fields.cloudlets, label: 'Cloudlets', formType: 'DualList', rules: { required: false }, visible: true, update: true, updateId: ['5', '5.1', '5.1.1', '5.1.2'], dependentData: [{ index: 1, field: fields.region }, { index: 2, field: fields.organizationName }] },
-            { field: fields.minActiveInstances, label: 'Min Active Instances (Required for HA)', formType: 'Input', rules: { type: 'number', required: false }, visible: true, updateId: ['6'], update: true, dataValidateFunc: this.validateMinInst, tip: 'Minimum number of active instances for High-Availability' },
+            { field: fields.minActiveInstances, label: 'Min Active Instances (Required for HA)', formType: 'Input', rules: { type: 'number', required: true, onBlur:true, requiredMsg:'Either Min Active Instances or Deploy Request Count is mandatory'  }, visible: true, updateId: ['6'], update: true, dataValidateFunc: this.validateMinInst, tip: 'Minimum number of active instances for High-Availability' },
             { field: fields.maxInstances, label: 'Max Instances', formType: 'Input', rules: { type: 'number', required: false }, visible: true, updateId: ['7'], update: true, dataValidateFunc: this.validateMaxInst, tip: 'Maximum number of instances (active or not)' },
         ]
     }
