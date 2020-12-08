fa0c32693401a76c90047748dc7bcdcf68bc8f53
Rahul.Jain <toniqapps@gmail.com>
2020-12-08 15:36:47 +0530

    Removed old monitoring

--- src/sites/siteFour/monitoring/mexmap/MapProperties.js
+++ src/sites/siteFour/monitoring/mexmap/MapProperties.js
@@ -1,7 +1,7 @@
 import * as L from "leaflet";
 
 export const cloudGreenIcon = L.icon({
-    iconUrl: require('../../monitoring/images/cloud_green.png'),
+    iconUrl: require('../helper/images/cloud_green.png'),
     iconSize: [40, 21],
     iconAnchor: [20, 21],
     shadowSize: [41, 41]
