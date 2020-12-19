2172dacc9577c5808e02b7d13bc38fd5d160ad11
rahuldsce <rahul.jain@mobiledgex.com>
2020-12-19 15:37:05 +0530

    head design (#759)

--- src/utils/date_util.js
+++ src/utils/date_util.js
@@ -1,5 +1,6 @@
 import * as moment from 'moment'
 import {getMexTimezone} from './sharedPreferences_util'
+import momentTimezone from "moment-timezone";
 
 export const FORMAT_FULL_DATE = 'YYYY-MM-DD'
 export const FORMAT_FULL_TIME = 'HH:mm:ss'
@@ -7,6 +8,7 @@ export const FORMAT_FULL_TIME_12 = 'hh:mm:ss'
 export const FORMAT_TIME_HH_mm = 'HH:mm'
 export const FORMAT_AM_PM = 'A'
 export const FORMAT_DAY = 'D'
+export const FORMAT_WD_TIME_HH_mm = `ddd, ${FORMAT_TIME_HH_mm}`
 export const FORMAT_FULL_DATE_TIME = `${FORMAT_FULL_DATE} ${FORMAT_FULL_TIME}`
 export const FORMAT_FULL_TIME_12_A = `${FORMAT_FULL_TIME_12} ${FORMAT_AM_PM}`
 export const FORMAT_DATE_24_HH_mm = `${FORMAT_FULL_DATE} ${FORMAT_TIME_HH_mm}`
