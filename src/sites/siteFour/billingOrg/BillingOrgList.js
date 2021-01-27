beaa560fa09ebadc7ee4a4f463939dc17f8143c3
rahuldsce <rahul.jain@mobiledgex.com>
2021-01-27 20:25:18 +0530

    E-4252 Web UI - Trust Policy permissions are not correct for users (#786)
    * Trust policy roles changed

--- src/sites/siteFour/billingOrg/BillingOrgList.js
+++ src/sites/siteFour/billingOrg/BillingOrgList.js
@@ -5,12 +5,10 @@ import * as actions from '../../../actions';
 import { connect } from 'react-redux';
 
 import MexListView from '../../../container/MexListView';
-import { fields, getUserRole } from '../../../services/model/format';
+import { fields } from '../../../services/model/format';
 import { showBillingOrg, deleteBillingOrg, keys } from '../../../services/model/billingOrg';
 import Reg from './BillingOrgReg';
-import * as constant from '../../../constant'
-import { HELP_ALERTS } from '../../../tutorial';
-
+import {validateRole, operatorRoles, BILLING_REMOVE_CHILD, BILLING_TYPE_PARENT, BILLING_ADD_CHILD, PAGE_BILLING_ORG} from '../../../constant'
 class BillingOrg extends React.Component {
     constructor(props) {
         super(props);
@@ -29,7 +27,7 @@ class BillingOrg extends React.Component {
     onReg = (action, data) => {
         let valid = true
         let actionId = action ? action.id : undefined
-        if (actionId === constant.BILLING_REMOVE_CHILD && data[fields.children] === undefined) {
+        if (actionId === BILLING_REMOVE_CHILD && data[fields.children] === undefined) {
             valid = false
         }
         if (valid) {
@@ -41,14 +39,14 @@ class BillingOrg extends React.Component {
     }
 
     orgActionVisible = (data) => {
-        return data[fields.type] === constant.BILLING_TYPE_PARENT.toLowerCase()
+        return data[fields.type] === BILLING_TYPE_PARENT.toLowerCase()
     }
 
     actionMenu = () => {
         return [
             // { label: 'Update', onClick: this.onAdd, type: 'Edit' },
-            { id: constant.BILLING_ADD_CHILD, label: 'Add Child', onClick: this.onReg, visible: this.orgActionVisible, icon: 'delete', warning: 'delete all the selected alerts', type: 'Edit' },
-            { id: constant.BILLING_REMOVE_CHILD, label: 'Remove Child', onClick: this.onReg, visible: this.orgActionVisible, icon: 'delete', warning: 'delete all the selected alerts', type: 'Edit' },
+            { id: BILLING_ADD_CHILD, label: 'Add Child', onClick: this.onReg, visible: this.orgActionVisible, icon: 'delete', warning: 'delete all the selected alerts', type: 'Edit' },
+            { id: BILLING_REMOVE_CHILD, label: 'Remove Child', onClick: this.onReg, visible: this.orgActionVisible, icon: 'delete', warning: 'delete all the selected alerts', type: 'Edit' },
             { label: 'Delete', onClick: deleteBillingOrg, type: 'Edit' }
         ]
     }
@@ -59,26 +57,16 @@ class BillingOrg extends React.Component {
         ]
     }
 
-    canAdd = () => {
-        let valid = false
-        let role = getUserRole();
-        if (role === constant.ADMIN_MANAGER || role === constant.OPERATOR_MANAGER || role === constant.OPERATOR_CONTRIBUTOR) {
-            valid = true
-        }
-        return valid
-    }
-
     requestInfo = () => {
         return ({
-            id: constant.PAGE_BILLING_ORG,
+            id: PAGE_BILLING_ORG,
             headerLabel: 'Billing Org',
             nameField: fields.name,
             requestType: [showBillingOrg],
             sortBy: [fields.name],
             selection: true,
-            viewMode: HELP_ALERTS,
             keys: this.keys,
-            onAdd: this.canAdd() ? this.onReg : undefined,
+            onAdd: validateRole(operatorRoles) ? this.onReg : undefined,
             grouping: false
         })
     }
