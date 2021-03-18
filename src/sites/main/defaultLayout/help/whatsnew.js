a8cf37b2e132bca26617dbeb89b22a0a5aef9856
Rahul.Jain <toniqapps@gmail.com>
2021-03-18 22:55:00 +0530

    whats new url changed

--- src/sites/main/defaultLayout/help/whatsnew.js
+++ src/sites/main/defaultLayout/help/whatsnew.js
@@ -11,15 +11,15 @@ const MexNew = (props) => {
     return (
         <Drawer anchor={'right'} open={props.open}>
             <div className='whats_new_container'>
-                <div style={{float:'left', marginTop:10, marginLeft:10}}>
-                    <h3>What's New</h3>
+                <div style={{float:'left', marginTop:10, marginLeft:10, fontSize:15}}>
+                    <h3><b>What's New</b></h3>
                 </div>
                 <div style={{float:'right'}}>
                     <IconButton onClick={close}>
                         <CloseIcon/>
                     </IconButton>
                 </div>
-                <object type="text/html"  data='https://developers.mobiledgex.com/release-notes/version2-4' width='100%' style={{height:'calc(96vh - 5px)'}}></object>
+                <object type="text/html"  data='https://developers.mobiledgex.com/release-notes/sdk' width='100%' style={{height:'calc(96vh - 15px)'}}></object>
             </div>
         </Drawer>
     )
