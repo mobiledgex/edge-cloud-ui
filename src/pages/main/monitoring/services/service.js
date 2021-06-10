523dea1836dcd7e49c58e5a3539e010431fddf3f
Rahul.Jain <toniqapps@gmail.com>
2021-06-10 23:08:03 +0530

    resource fields implmented for cluster instance

--- src/pages/main/monitoring/services/service.js
+++ src/pages/main/monitoring/services/service.js
@@ -39,8 +39,8 @@ export const getToken = (self) => {
 }
 
 export const checkExpiry = (self, message) => {
-    message = message.toLowerCase()
     if (message) {
+        message = message.toLowerCase()
         let isExpired = message.indexOf('expired jwt') > -1 || message.indexOf('expired token') > -1 || message.indexOf('token is expired') > -1
         if (isExpired && self) {
             setTimeout(() => {
