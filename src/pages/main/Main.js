0fcc94b363dd131ba07657494987e2e05724c4f7
Rahul.Jain <toniqapps@gmail.com>
2021-06-11 23:15:22 +0530

    pge redirect issue

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -48,31 +48,32 @@ class Main extends React.Component {
         }
     }
 
-    redirectInvalidPath = () => {
-        const orgInfo = this.props.organizationInfo
-        const pathname = this.props.history.location.pathname
-        let pathValid = false
-        if (pathname.includes('/logout')) {
-            pathValid = true
-        }
-        else {
-            for (let page of pages) {
+    validatePath = (pages, orgInfo, pathname) => {
+        for (let page of pages) {
+            if (page.sub) {
+                return this.validatePath(page.options, orgInfo, pathname)
+            }
+            else {
                 if (pathname.includes(page.path)) {
                     let roles = page.roles
                     if (roles) {
                         if (validateRole(page.roles, orgInfo)) {
-                            pathValid = true
+                            return true
                         }
                     }
                     else {
-                        pathValid = true
+                        return true
                     }
-                }
-                if (pathValid) {
-                    break;
                 }
             }
         }
+    }
+
+    redirectInvalidPath = () => {
+        const orgInfo = this.props.organizationInfo
+        const pathname = this.props.history.location.pathname
+        let pathValid = pathname.includes('/logout') || this.validatePath(pages, orgInfo, pathname)
+        
         if (!pathValid) {
             this.props.history.push(`/main/${PAGE_ORGANIZATIONS.toLowerCase()}`)
         }
