0e9ede63213879e51ec950e10d2afa85a42b0234
Venky <venky.tumkur@mobiledgex.com>
2021-07-05 14:36:32 +0530

    Add Google Analytics (#951)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import store from './store';
 import {Provider} from 'react-redux';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(process.env.REACT_APP_GA_MEASUREMENT_ID);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <Provider store={store} style={{width: '100%', height: '100%'}}>
