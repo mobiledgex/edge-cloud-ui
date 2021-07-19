b5bcc450729d832659908b33ee5f2b5e1e719d50
rahuldsce <rahul.jain@mobiledgex.com>
2021-07-19 14:45:50 +0530

    E-4446 [WebUI]Show Cloudlet Release info in Console and ShowNode (#963)
    * Cloudlet show node implemented

--- src/services/modules/cloudlet/primary.js
+++ src/services/modules/cloudlet/primary.js
@@ -1,5 +1,10 @@
 import { fields } from "../../model/format"
 
+
+export const CLOUDLET_ORG_NAME = 0
+export const CLOUDLET_ORG = 1
+export const CLOUDLET_NAME = 2
+
 const initialize = (parent, field, value) => {
   if (value) {
     parent = parent ? parent : {}
@@ -8,9 +13,14 @@ const initialize = (parent, field, value) => {
   return parent
 }
 
-export const primaryKeys = (data) => {
+export const primaryKeys = (data, include) => {
   let cloudletKey = undefined
-  cloudletKey = initialize(cloudletKey, 'name', data[fields.cloudletName])
-  cloudletKey = initialize(cloudletKey, 'organization', data[fields.operatorName])
+  include = include ? include : CLOUDLET_ORG_NAME
+  if (include === CLOUDLET_ORG_NAME || include === CLOUDLET_NAME) {
+    cloudletKey = initialize(cloudletKey, 'name', data[fields.cloudletName])
+  }
+  if (include === CLOUDLET_ORG_NAME || include === CLOUDLET_ORG) {
+    cloudletKey = initialize(cloudletKey, 'organization', data[fields.operatorName])
+  }
   return cloudletKey
 }
