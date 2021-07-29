81559e5d2cad8b62359976799d2b82f75e902836
Rahul.Jain <toniqapps@gmail.com>
2021-07-29 16:33:35 +0530

    Pull resolved

--- src/pages/main/events/auditLog/RightDrawer.js
+++ src/pages/main/events/auditLog/RightDrawer.js
@@ -83,10 +83,6 @@ const eventKeys = [
 const getKeys = (type) => {
     return type === 'audit' ? auditKeys : eventKeys
 }
-//{`App: ${mtags['app']} [${mtags['appver']}] -  ${mtags['apporg']}`}
-const Chip = (props) => (
-    <p variant="outlined" size="small" className='audit-log-label-chip'>{props.children}</p>
-)
 
 const Chip = (props) => (
     <p variant="outlined" size="small" className='audit-log-label-chip'>{props.children}</p>
