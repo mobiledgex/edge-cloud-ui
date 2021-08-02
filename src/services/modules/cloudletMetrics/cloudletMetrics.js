2289ac911cb02f1878c69dc98fa80170cd83ff69
Rahul.Jain <toniqapps@gmail.com>
2021-08-02 12:16:28 +0530

    VCD vshpere implemented

--- src/services/modules/cloudletMetrics/cloudletMetrics.js
+++ src/services/modules/cloudletMetrics/cloudletMetrics.js
@@ -29,35 +29,36 @@ export const cloudletFlavorMetricsKeys = [
 ]
 
 export const cloudletMetricsListKeys = [
-    { field: fields.region, label: 'Region', sortable: true, visible: false, groupBy: true  },
-    { field: fields.cloudletName, label: 'Cloudlet', sortable: true, visible: true, groupBy: true, customData:true  },
-    { field: fields.operatorName, label: 'Operator', sortable: true, visible: false, groupBy: true  },
-    { field: fields.cloudletLocation, label: 'Location', visible: false  },
+    { field: fields.region, label: 'Region', sortable: true, visible: false, groupBy: true },
+    { field: fields.cloudletName, label: 'Cloudlet', sortable: true, visible: true, groupBy: true, customData: true },
+    { field: fields.operatorName, label: 'Operator', sortable: true, visible: false, groupBy: true },
+    { field: fields.cloudletLocation, label: 'Location', visible: false },
     { field: 'cpu', label: 'vCpu Infra Usage', sortable: true, visible: true },
     { field: 'disk', label: 'Disk Infra Usage', sortable: true, visible: true },
     { field: 'memory', label: 'Memory Infra Usage', sortable: true, visible: true },
 ]
 
 export const utilizationMetricType = [
-    { field: 'cpu', serverField: 'utilization', subId: 'vCpuUsed', header: 'vCpu Infra Usage', position: 4, steppedLine:'after' },
-    { field: 'memory', serverField: 'utilization', subId: 'memUsed', header: 'Memory Infra Usage', position: 6, unit: UNIT_MB, steppedLine:'after' },
-    { field: 'disk', serverField: 'utilization', subId: 'diskUsed', header: 'Disk Infra Usage', position: 8, unit: UNIT_GB, steppedLine:'after' },
+    { field: 'cpu', serverField: 'utilization', subId: 'vCpuUsed', header: 'vCpu Infra Usage', position: 4, steppedLine: 'after' },
+    { field: 'memory', serverField: 'utilization', subId: 'memUsed', header: 'Memory Infra Usage', position: 6, unit: UNIT_MB, steppedLine: 'after' },
+    { field: 'disk', serverField: 'utilization', subId: 'diskUsed', header: 'Disk Infra Usage', position: 8, unit: UNIT_GB, steppedLine: 'after' },
 ]
 
 export const resourceUsageMetricType = [
-    { field: 'floatingIpsUsed', serverField: 'floatingIpsUsed', serverHead:'openstack-resource-usage', header: 'Floating IP Used', position: 4, unit:UNIT_FLOOR, steppedLine:'after' },
-    { field: 'gpusUsed', serverField: 'gpusUsed', serverHead:'openstack-resource-usage', header: 'GPU Used', position: 5, unit:UNIT_FLOOR, steppedLine:'after' },
-    { field: 'instancesUsed', serverField: 'instancesUsed', serverHead:'openstack-resource-usage', header: 'Instances Used', position: 6, unit:UNIT_FLOOR, steppedLine:'after' },
-    { field: 'ramUsed', serverField: 'ramUsed', serverHead:'openstack-resource-usage', header: 'RAM Used', position: 7, unit:UNIT_MB, steppedLine:'after' },
-    { field: 'vcpusUsed', serverField: 'vcpusUsed', serverHead:'openstack-resource-usage', header: 'vCPUs Used', position: 8, unit:UNIT_FLOOR, steppedLine:'after' },
+    { field: 'externalIpsUsed', serverField: 'externalIpsUsed', header: 'External IP Used', position: 4, unit: UNIT_FLOOR, steppedLine: 'after' },
+    { field: 'floatingIpsUsed', serverField: 'floatingIpsUsed', header: 'Floating IP Used', position: 5, unit: UNIT_FLOOR, steppedLine: 'after' },
+    { field: 'gpusUsed', serverField: 'gpusUsed', header: 'GPU Used', position: 6, unit: UNIT_FLOOR, steppedLine: 'after' },
+    { field: 'instancesUsed', serverField: 'instancesUsed', header: 'Instances Used', position: 7, unit: UNIT_FLOOR, steppedLine: 'after' },
+    { field: 'ramUsed', serverField: 'ramUsed', header: 'RAM Used', position: 8, unit: UNIT_MB, steppedLine: 'after' },
+    { field: 'vcpusUsed', serverField: 'vcpusUsed', header: 'vCPUs Used', position: 9, unit: UNIT_FLOOR, steppedLine: 'after' },
 ]
 
-export const cloudletMetricTypeKeys = ()=>([
-    { field: 'utilization', serverField: 'utilization', header: 'Memory Usage', keys:utilizationMetricType, serverRequest: endpoint.CLOUDLET_METRICS_ENDPOINT },
-    { field: 'resourceusage', serverField: 'resourceusage', header: 'Resource Usage', keys:resourceUsageMetricType, serverRequest: endpoint.CLOUDLET_METRICS_USAGE_ENDPOINT },
-    { field: 'count', header: 'Flavor Usage'},
+export const cloudletMetricTypeKeys = () => ([
+    { field: 'utilization', serverField: 'utilization', header: 'Memory Usage', keys: utilizationMetricType, serverRequest: endpoint.CLOUDLET_METRICS_ENDPOINT },
+    { field: 'resourceusage', serverField: 'resourceusage', header: 'Resource Usage', keys: resourceUsageMetricType, serverRequest: endpoint.CLOUDLET_METRICS_USAGE_ENDPOINT },
+    { field: 'count', header: 'Flavor Usage' },
     { field: 'map', header: 'Map' },
-    { field: 'event', header: 'Event'},
+    { field: 'event', header: 'Event' },
 ])
 
 export const cloudletMetrics = (data, org) => {
@@ -71,7 +72,6 @@ export const cloudletUsageMetrics = (data, org) => {
     data.cloudlet = data.cloudlet ? data.cloudlet : {
         organization: org
     }
-    data.platformtype = 'openstack'
     return { method: endpoint.CLOUDLET_METRICS_USAGE_ENDPOINT, data: data, keys: cloudletMetricsKeys }
 }
 
