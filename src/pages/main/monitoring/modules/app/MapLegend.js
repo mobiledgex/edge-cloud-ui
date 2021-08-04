ed4f048c8a613ef4510ba3e2d1afe48581c85c99
rahuldsce <rahul.jain@mobiledgex.com>
2021-08-04 16:18:54 +0530

    E 5422 Web UI - Latency App Info has misaligned data (#1014)
    * filter appinst-checkpoints
    
    * alignment resolved

--- src/pages/main/monitoring/modules/app/MapLegend.js
+++ src/pages/main/monitoring/modules/app/MapLegend.js
@@ -2,11 +2,20 @@ import { MapControl, withLeaflet } from "react-leaflet";
 import L from "leaflet";
 import { fields } from '../../../../../services/model/format';
 
+const tableKeys = [
+  { label: 'App', field: fields.appName },
+  { label: 'Version', field: fields.version },
+  { label: 'Cloudlet', field: fields.cloudletName },
+  { label: 'Operator', field: fields.operatorName },
+  { label: 'Cluster', field: fields.clusterName },
+]
+
 class Legend extends MapControl {
-  
+
+
+
   createLeafletElement(props) {
-    if(  props.data && props.data['main'] && props.data['main']['data'])
-    {
+    if (props.data && props.data['main'] && props.data['main']['data']) {
       this.data = props.data['main']['data']
     }
   }
@@ -17,27 +26,19 @@ class Legend extends MapControl {
       const div = L.DomUtil.create("div", "info legend");
       let labels = [];
 
-     
-      div.innerHTML = `\
-      <div style="background:#292C33;border-radius:5px;padding:10px;font-size:14px"><h4><b>App Info</b></h4>${labels.join("<br>")}\
-        <table>\
-          <tr>\
-            <td><code><b>App:</b></code></td><td><code>${this.data[fields.appName]}</code></td>\
-          </tr>\
-          <tr>\
-            <td><code><b>Version:</b></code></td><td>${this.data[fields.version]}</td>\
-          </tr>\
-          <tr>\
-            <td><code><b>Cloudlet:</b></code></td><td> ${this.data[fields.cloudletName]}</td>\
-          </tr>\
-          <tr>\
-            <td><code><b>Operator:</b></code></td><td> ${this.data[fields.operatorName]}</td>\
-          </tr>\
-          <tr>\
-            <td><code><b>Cluster:</b></code></td><td> ${this.data[fields.clusterName]}</td>\
-          </tr>\
-        </table>\
-      </div>`
+      let tabelView = `<div style="background:#292C33;border-radius:5px;padding:10px;font-size:14px"><h4><b>App Info</b></h4>${labels.join("<br>")}<table>`
+      tableKeys.forEach(key => (
+        tabelView = tabelView + `<tr>
+          <td style="width:100px">
+            <code><b>${key.label}:</b></code>
+          </td>
+          <td>
+            <code>${this.data[key.field]}</code>
+          </td>
+        </tr>`
+      ))
+      tabelView = tabelView + `</table></div>`
+      div.innerHTML = tabelView
       return div;
     };
 
@@ -45,7 +46,7 @@ class Legend extends MapControl {
     this.legend.addTo(map);
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     const { map } = this.props.leaflet;
     map.removeControl(this.legend)
   }
