aafbcbf13f46924cd1fc92b230ddddec311410fe
aparnatech <41115739+aparnatech@users.noreply.github.com>
2021-09-29 14:07:11 +0530

    E-5599 Added Logs button hide/show option in user preferences (#1074)
    * logs flow done
    
    * E-5599 Added Logs button hide/show option in user preferences
    
    * file rename

---
+++ src/pages/main/userSetting/preferences/LogsPref.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import MexForms, { SWITCH } from '../../../../hoc/forms/MexForms'
+import { Icon } from '../../../../hoc/mexui'
+import { PREF_LOGS } from '../../../../utils/sharedPreferences_util'
+
+const LogsPref = (props) => {
+  const [forms, setForms] = useState([])
+
+  useEffect(() => {
+    let forms = getforms()
+    let data = props.data
+    if (data) {
+      for (let i = 0; i < forms.length; i++) {
+        let form = forms[i]
+        form.value = data[form.field] !== undefined ? data[form.field] : form.value
+      }
+    }
+    setForms(forms)
+  }, []);
+
+  const onValueChange = (form) => {
+    let data = props.data
+    data[form.field] = form.value
+    props.update(data)
+  }
+  const getforms = () => (
+    [
+      { field: PREF_LOGS, label: 'Show Logs', value: true, formType: SWITCH, visible: true, tip: `Option to show/hide logs menu by default` },
+    ]
+  )
+  return (
+    <div style={{ paddingLeft: 15 }}>
+      <MexForms headerWidth={5} forms={forms} onValueChange={onValueChange} style={{ height: 400, width: 600 }} />
+    </div>
+  )
+}
+
+export default LogsPref
