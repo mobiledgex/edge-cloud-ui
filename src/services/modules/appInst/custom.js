15bd2acc64b19cf31fcbb21f5aa0787f2368fe4a
Rahul.Jain <toniqapps@gmail.com>
2021-11-07 22:39:44 +0530

    minor change

--- src/services/modules/appInst/custom.js
+++ src/services/modules/appInst/custom.js
@@ -3,17 +3,19 @@ import { fields } from "../../model/format"
 import { redux_org } from "../../../helper/reduxData"
 
 export const customize = (request, value, self = null) => {
-    value[fields.liveness] = value[fields.liveness]
-    value[fields.createdAt] = value[fields.createdAt] ? value[fields.createdAt][fields.seconds] : undefined
-    value[fields.updatedAt] = value[fields.updatedAt] ? value[fields.updatedAt][fields.seconds] : undefined
-    value[fields.ipAccess] = value[fields.ipAccess] ? ipAccess(value[fields.ipAccess]) : undefined
-    value[fields.revision] = value[fields.revision] ? value[fields.revision] : '0'
-    value[fields.healthCheck] = value[fields.healthCheck] ? value[fields.healthCheck] : 0
-    value[fields.sharedVolumeSize] = value[fields.autoClusterInstance] ? value[fields.sharedVolumeSize] ? value[fields.sharedVolumeSize] : 0 : undefined
-    value[fields.cloudlet_name_operator] = `${value[fields.cloudletName]} [${value[fields.operatorName]}]`
-    value[fields.app_name_version] = `${value[fields.appName]} [${value[fields.version]}]`
     if (value[fields.appName] === 'MEXPrometheusAppName' && !redux_org.isAdmin(self)) {
         value = undefined
+    }
+    else {
+        value[fields.liveness] = value[fields.liveness]
+        value[fields.createdAt] = value[fields.createdAt] ? value[fields.createdAt][fields.seconds] : undefined
+        value[fields.updatedAt] = value[fields.updatedAt] ? value[fields.updatedAt][fields.seconds] : undefined
+        value[fields.ipAccess] = value[fields.ipAccess] ? ipAccess(value[fields.ipAccess]) : undefined
+        value[fields.revision] = value[fields.revision] ? value[fields.revision] : '0'
+        value[fields.healthCheck] = value[fields.healthCheck] ? value[fields.healthCheck] : 0
+        value[fields.sharedVolumeSize] = value[fields.autoClusterInstance] ? value[fields.sharedVolumeSize] ? value[fields.sharedVolumeSize] : 0 : undefined
+        value[fields.cloudlet_name_operator] = `${value[fields.cloudletName]} [${value[fields.operatorName]}]`
+        value[fields.app_name_version] = `${value[fields.appName]} [${value[fields.version]}]`
     }
     return value
 }
