b868ebd19f84ad0109628ee93eb0d7688cade286
Rahul.Jain <toniqapps@gmail.com>
2021-11-15 23:01:01 +0530

    map implementation

--- src/pages/main/monitoring/common/map/Map.js
+++ src/pages/main/monitoring/common/map/Map.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { cloudGreenIcon } from '../../../../../hoc/mexmap/MapProperties'
 import MexMap from '../../../../../hoc/mexmap/MexMap'
+import { Marker, Popup } from "react-leaflet";
 import { fields } from '../../../../../services/model/format'
+import { Icon } from '../../../../../hoc/mexui';
 
 const DEFAULT_ZOOM = 3
 
@@ -8,17 +11,58 @@ class Map extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            backswitch: false
+            backswitch: false,
+            maps: undefined
         }
+        this.popup = React.createRef();
     }
 
     resetMap = () => {
     }
 
+    renderMarkerPopup = (data) => {
+        <Popup className="map-control-div-marker-popup" ref={this.popup}>
+            {
+                Object.keys(data).map(cloudletKey => {
+                    let dataList = data[cloudletKey]
+                    return (
+                        <div key={cloudletKey}>
+                            {
+                                dataList.map((item, i) => (
+                                    <div key={`${i}_${cloudletKey}`} className="map-control-div-marker-popup-label" >
+                                        <code style={{ fontWeight: 400, fontSize: 12 }}>
+                                            <Icon style={{ color: item.color, marginRight: 5 }}>circle</Icon>
+                                            {item[fields.cloudletName]}
+                                        </code>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )
+                })
+            }
+        </Popup>
+    }
+
     renderMarker = () => {
+        const { maps } = this.state
         return (
             <div>
-
+                {Object.keys(maps).map((key, i) => {
+                    let map = maps[key]
+                    let location = key.split('_')
+                    if (location.length === 2) {
+                        let lat = location[0]
+                        let lon = location[1]
+                        return (
+                            <React.Fragment key={key}>
+                                <Marker icon={cloudGreenIcon(Object.keys(map).length)} position={[lat, lon]}>
+                                    {this.renderMarkerPopup(map)}
+                                </Marker>
+                            </React.Fragment>
+                        )
+                    }
+                })}
             </div>
         )
     }
@@ -26,34 +70,35 @@ class Map extends React.Component {
     static getDerivedStateFromProps(props, state) {
         if (props.data) {
             const { data, tools } = props
-            let mapData = {}
+            let maps = {}
             let keys = Object.keys(data)
-            keys.map(key=>{
+            keys.map(key => {
                 let item = data[key]
-                const {latitude, longitude} = item[fields.cloudletLocation]
-                let mapkey = `${latitude}_${longitude}`
-                mapData[mapkey] = mapData[mapkey] ? mapData[mapkey] : {}
-                mapData[mapkey] = item
+                const { latitude, longitude } = item[fields.cloudletLocation]
+                let mapKey = `${latitude}_${longitude}`
+                let cloudletKey = item[fields.cloudletName]
+                maps[mapKey] = maps[mapKey] ? maps[mapKey] : {}
+                maps[mapKey][cloudletKey] = maps[mapKey][cloudletKey] ? maps[mapKey][cloudletKey] : []
+                maps[mapKey][cloudletKey].push(item)
             })
-            console.log(mapData)
+            return { maps }
         }
         return null
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        return this.props.refresh !== nextProps.refresh
+        return true//this.props.refresh !== nextProps.refresh
     }
 
     render() {
-        const { backswitch } = this.state
+        const { backswitch, maps } = this.state
         const { regions } = this.props
         return (
-            <MexMap renderMarker={this.renderMarker} back={this.resetMap} zoom={DEFAULT_ZOOM} backswitch={backswitch} region={regions} />
+            maps ? <MexMap renderMarker={this.renderMarker} back={this.resetMap} zoom={DEFAULT_ZOOM} backswitch={backswitch} region={regions} /> : null
         )
     }
 
-    componentDidMount(){
-        console.log(this.props.legends)
+    componentDidMount() {
     }
 }
 
