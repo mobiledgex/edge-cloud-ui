8f8f67e9f961524724c55904f4bacd657e718b63
Rahul <rahul.jain@mobiledgex.com>
2021-11-30 17:50:08 +0530

    Code fix

--- src/container/MexListView.js
+++ src/container/MexListView.js
@@ -640,8 +640,7 @@ const mapDispatchProps = (dispatch) => {
     return {
         handleAlertInfo: (mode, msg) => { dispatch(actions.alertInfo(mode, msg)) },
         handleLoadingSpinner: (data) => { dispatch(actions.loadingSpinner(data)) },
-        handleViewMode: (data) => { dispatch(actions.viewMode(data)) },
-        handleRoleInfo: (data) => { dispatch(actions.roleInfo(data)) }
+        handleViewMode: (data) => { dispatch(actions.viewMode(data)) }
     };
 };
 
