b39dbeb8a6ad6b58a61d2226468a45fbaf71b2db
Rahul <rahul.jain@mobiledgex.com>
2021-12-01 23:22:43 +0530

    Token validation

--- src/services/service.js
+++ src/services/service.js
@@ -237,13 +237,12 @@ export const multiAuthSyncRequest = async (self, requestList, format = true) => 
 
         try {
             let responseList = await axios.all(promise)
-            console.log('Rahul1234try',responseList)
             responseList.forEach((response, i) => {
                 resResults.push(formatter(requestList[i], response, format));
             })
         }
         catch (error) {
-            console.log('Rahul1234error',error)
+            alert(error)
             errorResponse(self, requestList[0], error)
         }
         showProgress(self)
