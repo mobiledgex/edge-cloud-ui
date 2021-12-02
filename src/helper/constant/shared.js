418d9751cee8fec988900b95e90fad43b7cd69c5
Rahul <rahul.jain@mobiledgex.com>
2021-12-02 14:40:26 +0530

    CSP testing

--- src/helper/constant/shared.js
+++ src/helper/constant/shared.js
@@ -1,5 +1,10 @@
 import { perpetual } from "."
 
-export const isPathOrg = (self)=>{
+export const isPathOrg = (self) => {
     return self.props.history.location.pathname.includes(perpetual.PAGE_ORGANIZATIONS.toLowerCase())
+}
+
+export const tokenRequired = () => {
+    const tokenReq = ['localhost', 'kubernetes.docker.internal']
+    return tokenReq.includes(window.location.hostname)
 }
