7fb2599ab3fdb31c0053169e728c7b60653c39f7
Rahul Jain <rahul.jain@mobiledgex.com>
2022-02-19 18:28:27 +0530

    Pool fixed

--- src/pages/main/federation/reg/Federator.js
+++ src/pages/main/federation/reg/Federator.js
@@ -82,7 +82,7 @@ class RegisterOperator extends React.Component {
             { label: `${this.isUpdate ? 'Update' : 'Enter'} Operator Details`, formType: MAIN_HEADER, visible: true },
             { field: fields.region, label: 'Region', formType: SELECT, placeholder: 'Select Region', rules: { required: true }, visible: true, update: { key: true } },
             { field: fields.operatorName, label: 'Operator', formType: this.isUpdate || redux_org.nonAdminOrg(this) ? INPUT : SELECT, placeholder: 'Select Operator', rules: { required: true, disabled: !redux_org.isAdmin(this) }, visible: true, value: redux_org.nonAdminOrg(this), tip: 'Organization of the federation site', update: { key: true } },
-            { field: fields.countryCode, label: ' Country Code', formType: INPUT, placeholder: 'Enter Country Code', rules: { required: true }, visible: true, tip: 'ISO 3166-1 Alpha-2 code for the country where operator platform is located' },
+            { field: fields.countryCode, label: ' Country Code', formType: INPUT, placeholder: 'Enter Country Code', rules: { required: true, type:'search' }, visible: true, tip: 'ISO 3166-1 Alpha-2 code for the country where operator platform is located' },
             this.isUpdate ? { field: fields.federationId, label: 'Federation ID', formType: INPUT, visible: true, tip: 'Self federation ID' } :
                 { uuid: uniqueId(), field: fields.federationId, label: 'Federation ID', formType: INPUT, visible: true, forms: this.federationIdElements(), tip: 'Self federation ID' },
             { field: fields.locatorendpoint, label: 'Locator End Point', formType: INPUT, placeholder: 'Enter Locator Endpoint', visible: true, update: { edit: true }, tip: 'IP and Port of discovery service URL of operator platform' },
