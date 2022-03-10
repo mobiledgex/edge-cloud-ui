1f08aa6f55f983b03c68d482f5afe7da39b02892
Rahul Jain <rahul.jain@mobiledgex.com>
2022-03-10 16:40:40 +0530

    E-6224 Audit Log shouldn't log ShowAlert response (#1230)
    * Show Alert Expiry Fixed

--- src/services/worker/interceptor.js
+++ src/services/worker/interceptor.js
@@ -1,6 +1,6 @@
 import { validateExpiry } from "../config"
 
-export const postMessage = (worker, message) => new Promise((resolve, reject) => {
+export const postMessage = (worker, message, self) => new Promise((resolve, reject) => {
     const resolution = (event) => {
         let response = event.data
         worker.removeEventListener('message', resolution)
@@ -17,9 +17,9 @@ export const postMessage = (worker, message) => new Promise((resolve, reject) =>
     worker.postMessage(message)
 })
 
-export const processWorker = async (worker, object) => {
+export const processWorker = async (self, worker, object) => {
     try {
-        return await postMessage(worker, { ...object })
+        return await postMessage(worker, { ...object }, self)
     }
     catch (e) {
         return undefined
