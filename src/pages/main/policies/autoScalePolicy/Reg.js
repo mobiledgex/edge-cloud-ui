68f2704721f773464f54c988abcb0f43fc71edd5
Rahul Jain <rahul.jain@mobiledgex.com>
2022-03-16 02:03:19 +0530

    label error fixed (#1237)

--- src/pages/main/policies/autoScalePolicy/Reg.js
+++ src/pages/main/policies/autoScalePolicy/Reg.js
@@ -64,7 +64,7 @@ class AutoScalePolicyReg extends React.Component {
         if (currentForm.value && currentForm.value.length > 0) {
             let value = parseInt(currentForm.value)
             if (value <= 0 || value > 100) {
-                currentForm.error = 'Scale threshold must be between 1 and 100'
+                currentForm.error = `${currentForm.label} valid range should be between 1 and 100`
                 return false;
             }
         }
@@ -91,9 +91,9 @@ class AutoScalePolicyReg extends React.Component {
         { field: fields.minimumNodes, label: 'Minimum Nodes', formType: INPUT, placeholder: 'Enter Minimum Nodes', rules: { type: 'number', required: true, onBlur: true }, visible: true, update: { id: ['3'] }, dataValidateFunc: this.validateNodes, tip: 'Minimum number of cluster nodes' },
         { field: fields.maximumNodes, label: 'Maximum Nodes', formType: INPUT, placeholder: 'Enter Maximum Nodes', rules: { type: 'number', required: true, onBlur: true }, visible: true, update: { id: ['4'] }, dataValidateFunc: this.validateNodes, tip: 'Maximum number of cluster nodes' },
         { field: fields.stabilizationWindowSec, label: 'Stabilization Window (sec)', formType: INPUT, placeholder: 'Enter Stabilization Window In Seconds', unit: 'sec', visible: true, rules: { type: 'number', required: true }, update: { id: ['8'] }, tip: 'Stabilization window is the time for which past triggers are considered; the largest scale factor is always taken.' },
-        { field: fields.targetCPU, label: 'Target CPU', formType: INPUT, placeholder: 'Enter Target CPU', rules: { type: 'number' }, unit: '%', visible: true, dataValidateFunc: this.validateThreshold, update: { id: ['9'] }, tip: 'Target per-node cpu utilization (percentage 1 to 100), 0 means disabled' },
-        { field: fields.targetMEM, label: 'Target Memory', formType: INPUT, placeholder: 'Enter Target Memory', rules: { type: 'number' }, unit: '%', visible: true, dataValidateFunc: this.validateThreshold, update: { id: ['10'] }, tip: 'Target per-node memory utilization (percentage 1 to 100), 0 means disabled' },
-        { field: fields.targetActiveConnections, label: 'Target Active Connections', formType: INPUT, placeholder: 'Enter Target Active Connections', visible: true, rules: { type: 'number' }, update: { id: ['11'] }, tip: 'Target per-node number of active connections, 0 means disabled' },
+        { field: fields.targetCPU, label: 'Target CPU', formType: INPUT, placeholder: 'Enter Target CPU', rules: { type: 'number' }, unit: '%', visible: true, dataValidateFunc: this.validateThreshold, update: { id: ['9'] }, tip: 'Target per-node cpu utilization (percentage between 1 to 100)' },
+        { field: fields.targetMEM, label: 'Target Memory', formType: INPUT, placeholder: 'Enter Target Memory', rules: { type: 'number' }, unit: '%', visible: true, dataValidateFunc: this.validateThreshold, update: { id: ['10'] }, tip: 'Target per-node memory utilization (percentage between 1 to 100)' },
+        { field: fields.targetActiveConnections, label: 'Target Active Connections', formType: INPUT, placeholder: 'Enter Target Active Connections', visible: true, rules: { type: 'number' }, update: { id: ['11'] }, tip: 'Target per-node number of active connections' },
     ])
 
 
