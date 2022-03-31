b5a24588f6ec145f6ce053fec87133c5ffece2e6
Rahul Jain <rahul.jain@mobiledgex.com>
2022-04-01 01:44:20 +0530

    Reload fixed on org change

--- src/pages/main/dashboard/Dashboard.js
+++ src/pages/main/dashboard/Dashboard.js
@@ -1,23 +1,43 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { EVENT } from '../../../helper/constant/perpetual'
+import { uniqueId } from '../../../helper/constant/shared'
+import { localFields } from '../../../services/fields'
 import Logs from './auditLog/Logs'
 import Control from './control/Control'
 
 class Dashboard extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            renderKey: uniqueId()
+        }
     }
-    
+
     render() {
+        const { renderKey } = this.state
         return (
-            <div className='dashboard'>
+            <div className='dashboard' key={renderKey}>
                 <Control>
-                    <Logs type={EVENT}/>
+                    <Logs type={EVENT} />
                 </Control>
             </div>
         )
-    }   
+    }
+
+    componentDidUpdate(preProps, preState) {
+        const { organizationInfo } = this.props
+        if (preProps.organizationInfo[localFields.organizationName] !== organizationInfo[localFields.organizationName]) {
+            this.setState({ renderKey: uniqueId() })
+        }
+    }
 }
 
-export default withRouter(Dashboard)
+const mapStateToProps = (state) => {
+    return {
+        organizationInfo: state.organizationInfo.data
+    }
+};
+
+export default connect(mapStateToProps, null)(Dashboard);
