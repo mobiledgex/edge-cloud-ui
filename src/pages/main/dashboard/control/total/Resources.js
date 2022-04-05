c23d6025f08c6edc32d6b4da7c4cf0058028da2c
Rahul Jain <rahul.jain@mobiledgex.com>
2022-04-05 11:52:46 +0530

    Resources fixed (#1276)

--- src/pages/main/dashboard/control/total/Resources.js
+++ src/pages/main/dashboard/control/total/Resources.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { makeStyles } from '@material-ui/core'
 import clsx from 'clsx'
 import { MexIcon } from '../../../../../helper/formatter/ui'
-
+import './style.css'
 const useStyles = makeStyles(theme => (
     {
         resources: {
@@ -27,15 +27,34 @@ const useStyles = makeStyles(theme => (
 const Resources = (props) => {
     const { data } = props
     const classes = useStyles()
+    const graph = useRef(null);
+
+    useEffect(() => {
+        const circlechart = graph.current;
+        const circleElements = circlechart.childNodes;
+    
+        let angle = 360 - 90;
+        let dangle = 360 / circleElements.length;
+    
+        for (let circle of circleElements) {
+          angle += dangle;
+          circle.style.transform = `rotate(${angle}deg) translate(${circlechart.clientWidth /
+            2}px) rotate(-${angle}deg)`;
+        }
+      }, []);
+
+      
     return (
-        <div className={classes.resources}>
+        <div className="circlechart" ref={graph}>
             {
-                Object.keys(data).map((key) => {
+                Object.keys(data).map((key, i) => {
                     let item = data[key]
                     return (
-                        item.label ? <div key={key} className={clsx('mex-card', classes.content)} >
-                            <MexIcon name={item.icon ?? 'ip_icon.svg'} />
-                            <h4 className={classes.label}>{`${item.label} - ${item.value}`}</h4>
+                        item.label ? <div key={key} className={clsx('mex-card', 'circle')} >
+                            <div align='center'>
+                                <MexIcon name={item.icon ?? 'ip_icon.svg'} />
+                                <h4 className={classes.label}>{`${item.label} - ${item.value}`}</h4>
+                            </div>
                         </div> : null
                     )
                 })
